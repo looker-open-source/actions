69ddaebf85dfadb5b0a301395d96ce6eb0382844
Wil Gieseler <wil@wilgieseler.com>
2017-07-21 12:32:26 -0700

    disable winston logging in tests

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,11 @@
 import * as chai from "chai"
+import * as winston from "winston"
+import ChaiHttp = require("chai-http")
+
+chai.use(ChaiHttp)
+winston.remove(winston.transports.Console)
 
 import "../src/integrations/index"
-
-import ChaiHttp = require("chai-http")
-chai.use(ChaiHttp)
 
 import "./test_server"
 import "./test_smoke"
