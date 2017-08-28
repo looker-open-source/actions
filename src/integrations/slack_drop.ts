2d52bb838fbd64cd9df92e12dcab99317d0b05e0
joerozo <joe.rozo@productops.com>
2017-08-28 11:07:55 -0700

    Finished slack_drop

--- src/integrations/slack_drop.ts
+++ src/integrations/slack_drop.ts
@@ -52,54 +52,39 @@ export class SlackFileDrop extends D.Integration {
         reject("Missing parameters")
       }
 
-
-
       const slack = new Slack(request.params.token)
 
-
       /********************************************************
-       *  When running this integration (and the Azure one)
-       *  the method that creates the file (fs.writeFile)
-       *  needs to be run and then commented out for the
-       *  second one to run properly (slack.uploadFile)
-       *  I think this is a synchronicity problem --> need to
-       *  check with Eric should execute the top method and then()
-       *  execute the second one --> this might also have to
-       *  do with the fact that the first method includes
-       *  resolve and reject? should be resolved upon completion
-       *  of the second method 
-       *
-       *  Also ... this is returning type Json should be type CSV
+       *   this is returning type Json should be type CSV
        *  run this integration on the server first and see what format
        *  of attachment is automatically included by Looker
        ************************************************************/
 
+      var file_upload = function(){
+        slack.uploadFile({
+          file: fs.createReadStream(path.join("./", "attached_file.txt")),
+          filetype: 'text',
+          title: 'looker_attached_file',
+          initialComment: 'File added by Looker',
+          channels: '#integrations_channel'
+        }, function(err:any , data:any){
+          if(!err){
+            winston.info('Uploaded file details: ', data)
+            fs.unlinkSync('./attached_file.txt')
+            resolve(data)
+          }
+          winston.info("Slack File Upload Failed", err)
+          reject(err)
+        })
+      }
 
-      // const attached_file = "attached_file.txt"
-      // const qr = JSON.stringify(request.attachment)
-      // fs.writeFile(attached_file, qr, function(err: any){
-      //   if(!err){
-      //     resolve()
-      //     winston.info("Failure")
-      //   }
-      //   reject()
-      //   winston.info("Success")
-      // })
-
-      slack.uploadFile({
-        file: fs.createReadStream(path.join("./", "attached_file.txt")),
-        filetype: 'text',
-        title: 'looker_attached_file',
-        initialComment: 'it worked !! ',
-        channels: '#integrations_channel'
-      }, function(err:any , data:any){
+      const attached_file = "attached_file.txt"
+      const qr = JSON.stringify(request.attachment)
+      fs.writeFile(attached_file, qr, function(err: any){
         if(!err){
-          winston.info('Uploaded file details: ', data)
-          fs.unlinkSync('./attached_file.txt')
-          resolve(data)
+          file_upload()
         }
-        winston.info("It failed", err)
-        reject(err)
+        winston.info("Failure")
       })
     })
   }
