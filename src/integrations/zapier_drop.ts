cb029f350ad6014e6ed7e6f336b40c8957d41917
joerozo <joe.rozo@productops.com>
2017-08-31 11:29:08 -0700

    Amended Azure integrations with comments to follow flow, adding zapier_drop integration and amending index.ts

--- src/integrations/zapier_drop.ts
+++ src/integrations/zapier_drop.ts
@@ -1,42 +1,50 @@
 import * as winston from "winston"
 import * as D from "../framework"
 
-const azure = require("azure-storage")
+const req = require("request")
 // import * as azure from 'aws-sdk/clients/ec2'
 
 /********************************************************************
- * This Integration Assumes you have an Azure account. Given an account
- * and an access key the integration logs you in. It then creates a
- * storage container called integrationscontainer (if one doesn't already
- * exist) and then writes the looker attachment as a blobfile titled qrBlob
- * to the container.
- *********************************************************************/
+ *  The majority of this integration is built into the Zapier app    *
+ *  whatever "Zaps" you build this integration will send the data    *
+ *  attachment to Zapier. Must first generate a Zapier Webhhook url  *
+ *  (endpoint for your data post) --> Simply create a new zap -      *
+ *  built the first trigger as a webhhook --> willgive you a url     *
+ *  endpoint                                                         *
+*********************************************************************/
 
 export class AzureStorageIntegration extends D.Integration {
 
   allowedTags = ["aws_resource_id", "aws_region"]
   constructor() {
     super()
-    this.name = "azure_storage"
-    this.label = "Azure Storage"
-    this.iconName = "azure_storage.svg"
-    this.description = "Write data files to an Azure container."
+    this.name = "zapier_drop"
+    this.label = "Zapier Integrations Work Flow"
+    this.iconName = "Zapier.svg"
+    this.description = "Takes a data attachment and begins a Zapier workflow"
     this.supportedActionTypes = ["query", "cell"]
     this.requiredFields = []
     this.params = [
       {
-        name: "account",
-        label: "Storage Account",
+        name: "user",
+        label: "User email account",
         required: true,
-        description: "Your account for Azure.",
+        description: "Your email login for Zapier",
         sensitive: false,
       },
       {
-        name: "accessKey",
-        label: "Access Key",
+        name: "password",
+        label: "password",
         required: true,
         sensitive: true,
-        description: "Your access key for Azure.",
+        description: "Your Zapier account password",
+      },
+      {
+        name: "url",
+        label: "Zapier URL EndPoint",
+        required: true,
+        sensitive: true,
+        description: "The WebHook URL endpoint for your Zaps",
       },
     ]
     this.supportedFormats = ["json_detail"]
@@ -48,6 +56,9 @@ export class AzureStorageIntegration extends D.Integration {
     return new Promise<D.DataActionResponse>((resolve, reject) => {
       // containerName must be the name of an existing blob container in Azure storage
       // const containerName = "integrationscontainer"
+
+      const url_hook = request.params.url.toString()
+      winston.info("URL URL URL", url_hook)
       winston.info(JSON.stringify(request.attachment))
 
       if (!(request.attachment && request.attachment.dataJSON)) {
@@ -68,32 +79,18 @@ export class AzureStorageIntegration extends D.Integration {
         reject("Missing Correct Parameters")
       }
 
-      const containerName = "integrationscontainer"
-      const blobService = azure.createBlobService(request.params.account, request.params.accessKey)
-      winston.info(blobService)
-      blobService.createContainerIfNotExists(containerName, { publicAccessLevel: "blob"}, (error: any) => {
+      req({
+        url: url_hook,
+        method: "POST",   // <--Very important!!!use
+        body: qr,
+      }, function(error: any, response: any){
         if (!error){
-          blob_write()
+          winston.info("Success")
+          resolve(response)
         }
-        winston.info("Error", error)
+        reject(error)
       })
 
-      let blob_write = function(){
-        blobService.createBlockBlobFromText(
-            containerName,
-            "qrBlob",
-            qr,
-            function(error: any){
-              if (error){
-                winston.info("Couldn't upload string")
-                winston.info(error)
-                reject()
-              } else {
-                winston.info("Uploaded Successfully")
-                resolve()
-              }
-            })
-      }
     })
   }
 }
