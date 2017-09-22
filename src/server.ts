38b1cd9c733b2cfd3f38978bc0d1e1330a756c64
Jamie Davidson <jdavidson@gmail.com>
2017-09-22 09:43:14 -0700

    better naming for shadowed path

--- src/server.ts
+++ src/server.ts
@@ -68,8 +68,8 @@ export class Server {
 
   }
 
-  private route(p: string, fn: (req: express.Request, res: express.Response) => void): void {
-    this.app.post(p, async (req, res) => {
+  private route(urlPath: string, fn: (req: express.Request, res: express.Response) => void): void {
+    this.app.post(urlPath, async (req, res) => {
       winston.info(`Starting request for ${req.url}`)
 
       const tokenMatch = (req.header("authorization") || "").match(TOKEN_REGEX)
