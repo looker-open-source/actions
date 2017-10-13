eab1f60cf4df151f6f4cf665a7d5ae8b35c17c36
Jamie Davidson <jdavidson@gmail.com>
2017-10-13 08:31:23 -0700

    add form test

--- test/integrations/test_jira.ts
+++ test/integrations/test_jira.ts
@@ -17,12 +17,6 @@ function expectJiraNewIssueMatch(request: D.DataActionRequest, match: any) {
       return {
         addNewIssue: addNewIssueSpy,
         addAttachmentOnIssue: addAttachmentonIssueSpy,
-        listProjects: () => [
-          {id: "1", name: "A"},
-          {id: "2", name: "B"}],
-        listIssueTypes: () => [
-          {id: "1", name: "Bug"},
-          {id: "2", name: "Request"}],
       }
     })
 
@@ -37,7 +31,7 @@ describe(`${integration.constructor.name} unit tests`, () => {
 
   describe("action", () => {
 
-    it("sends right body to key and bucket", () => {
+    it("sends correct jira new issue", () => {
       const request = new D.DataActionRequest()
       request.scheduledPlan = {url: "looker_url"}
       request.formParams = {
@@ -69,6 +63,48 @@ describe(`${integration.constructor.name} unit tests`, () => {
       chai.expect(integration.hasForm).equals(true)
     })
 
+    it("has form with correct projects and issues", (done) => {
+      const stubClient = sinon.stub(integration as any, "jiraClientFromRequest")
+        .callsFake(() => {
+          return {
+            listProjects: () => [
+              {id: "1", name: "A"},
+              {id: "2", name: "B"}],
+            listIssueTypes: () => [
+              {id: "1", name: "Bug", description: "x"},
+              {id: "2", name: "Request"}],
+          }
+        })
+      const request = new D.DataActionRequest()
+      const form = integration.validateAndFetchForm(request)
+      chai.expect(form).to.eventually.deep.equal({
+        fields: [{
+          default: "1",
+          label: "Project",
+          name: "project",
+          options: [{name: "1", label: "A"}, {name: "2", label: "B"}],
+          type: "select",
+          required: true,
+        }, {
+          label: "Summary",
+          name: "summary",
+          type: "string",
+          required: true,
+        }, {
+          label: "Description",
+          name: "description",
+          type: "textarea",
+          required: true,
+        }, {
+          default: "1",
+          label: "Issue Type",
+          name: "issueType",
+          type: "select",
+          options: [{name: "1", label: "Bug"}],
+          required: true,
+        }],
+      }).and.notify(stubClient.restore).and.notify(done)
+    })
   })
 
 })
