7271bb74214c206bb3be7805c974476d431983f9
Jamie Davidson <jdavidson@gmail.com>
2017-11-07 14:33:40 -0800

    rework label

--- src/integrations/twilio/twilio.ts
+++ src/integrations/twilio/twilio.ts
@@ -11,7 +11,7 @@ export class TwilioIntegration extends D.Integration {
     super()
 
     this.name = "twilio"
-    this.label = "Twilio - Send Notification"
+    this.label = "Twilio - Send Data"
     this.iconName = "twilio/twilio.svg"
     this.description = "Send data from a Look to a phone number via Twilio."
     this.supportedActionTypes = ["query"]
