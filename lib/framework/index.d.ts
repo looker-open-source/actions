cd6a67e5683a80b48eb1b6f018104f7638e04750
Jamie Davidson <jdavidson@gmail.com>
2018-03-22 10:17:20 -0700

    remove autogenerated lib

--- lib/framework/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./action_form";
-export * from "./action_request";
-export * from "./action_response";
-export * from "./integration";
-export * from "./sources";
-export * from "./utils";
