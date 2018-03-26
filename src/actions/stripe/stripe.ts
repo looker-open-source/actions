2f94762b752a396f0f35fd812c72753b8080abc9
Elijah Tabb <elijah@looker.com>
2018-03-26 12:41:57 -0700

    first step

---
+++ src/actions/stripe/stripe.ts
@@ -0,0 +1,45 @@
+import * as Hub from "../../hub"
+
+import * as stripe from "stripe"
+
+const TAG = "stripe_charge_id"
+
+export class StripeRefundAction extends Hub.Action {
+
+    name = "stripe_refund_charge"
+    label = "Stripe - Refund Charge"
+    iconName = "amazon/amazon_ec2.png"
+    description = "Refund a Stripe charge."
+    params = [
+        {
+            name: "secret_key",
+            label: "Secret Key",
+            required: true,
+            sensitive: true,
+            description: "Your secret key for Stripe.",
+        },
+    ]
+    supportedActionTypes = [Hub.ActionType.Cell]
+    supportedFormats = [Hub.ActionFormat.JsonDetail]
+    requiredFields = [{ tag: TAG }]
+
+    async execute(request: Hub.ActionRequest) {
+        stripe_client = stripe(request.params.secret_key)
+        let charge_id: string
+        if (!request.params.value) {
+            throw "Couldn't get data from cell."
+        }
+        charge_id = request.params.value
+        const params = {charge: charge_id}
+
+        let response
+        try {
+            await stripe_client.refunds.create(params)
+        } catch (e) {
+            response = {success: false, message: e.message}
+        }
+        return new Hub.ActionResponse(response)
+    }
+}
+
+Hub.addAction(new StripeRefundAction())
