43b25db0ac4b17c50ae872fac16d4ecbc3b1a094
nickfogler <nick@4mile.io>
2018-05-04 18:09:08 -0700

    Uninstall instructions text

--- src/actions/workplace/uninstall.ts
+++ src/actions/workplace/uninstall.ts
@@ -1,3 +1,22 @@
 export function uninstallWorkplace(_req: any, res: any) {
   res.render("uninstall")
 }
+
+// Uninstall Instructions
+
+// To uninstall the Workplace by Facebook integration from Looker, navigate to the Actions admin settings in your Looker instance.
+//
+// Click "Edit" on the Workplace by Facebook integration.
+//
+// [first image from install page]
+//
+// Toggle the "Enabled" button to the off position.
+//
+// Click "Edit" next to the field "Facebook App Access Token", and ensure that there is no text stored in the text field.
+//
+// Click "Save".
+//
+// [second image from install page]
+//
+
+
