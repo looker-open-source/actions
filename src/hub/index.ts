9e6c2257814c2ebe2d1e040115fe0114e88258f4
Wil Gieseler <wil@wilgieseler.com>
2018-05-25 12:45:36 -0700

    add allFields function and Field and Fieldset imports

--- src/hub/index.ts
+++ src/hub/index.ts
@@ -5,5 +5,25 @@ export * from "./action"
 export * from "./sources"
 export * from "./utils"
 
+import { LookmlModelExploreField as Field } from "../api_types/lookml_model_explore_field"
+import { LookmlModelExploreFieldset as Fieldset } from "../api_types/lookml_model_explore_fieldset"
 import * as JsonDetail from "./json_detail"
-export { JsonDetail }
+
+function allFields(fields: Fieldset) {
+  let all: Field[] = []
+  if (fields.dimensions) {
+    all = all.concat(fields.dimensions)
+  }
+  if (fields.measures) {
+    all = all.concat(fields.measures)
+  }
+  if (fields.filters) {
+    all = all.concat(fields.filters)
+  }
+  if (fields.parameters) {
+    all = all.concat(fields.parameters)
+  }
+  return all
+}
+
+export { JsonDetail, Field, Fieldset, allFields }
