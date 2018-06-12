34bafb02251c184a5dd9b0c9b59a4e99f60d4315
Wil Gieseler <wilg@users.noreply.github.com>
2018-06-12 15:48:32 -0700

    Import Slack so that type definitions come through (#155)
    this masked the bug from https://github.com/looker/actions/pull/154/files

--- src/actions/slack/slack.ts
+++ src/actions/slack/slack.ts
@@ -1,6 +1,6 @@
 import * as Hub from "../../hub"
 
-const WebClient = require("@slack/client").WebClient
+import { WebClient } from "@slack/client"
 
 interface Channel {
   id: string,
@@ -94,8 +94,8 @@ export class SlackAttachmentAction extends Hub.Action {
     return new Promise<Channel[]>((resolve, reject) => {
       const slack = this.slackClientFromRequest(request)
       slack.channels.list({
-        exclude_archived: 1,
-        exclude_members: 1,
+        exclude_archived: true,
+        exclude_members: true,
       }, (err: any, response: any) => {
         if (err || !response.ok) {
           reject(err)
