b4b21ac2aba27c074cf29a75daae3622dc8e0b3b
Phillip Peralez <pwperalez@gmail.com>
2018-09-04 13:46:36 -0700

    Lint error

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,8 +6,8 @@ import * as path from "path"
 import * as Raven from "raven"
 import * as winston from "winston"
 import * as Hub from "../hub"
-import * as ExecuteProcessQueue from "./execute_process_queue"
 import * as apiKey from "./api_key"
+import * as ExecuteProcessQueue from "./execute_process_queue"
 
 const expressWinston = require("express-winston")
 const blocked = require("blocked-at")
