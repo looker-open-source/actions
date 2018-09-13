c1e5ea90dff3bcd4c74d790d9b243ea0c1fda915
phillipperalez <pwperalez@gmail.com>
2018-09-13 15:18:40 -0700

    adding webhookId to logging (#205)
    * adding webhookId to logging
    
    * Linter fixes

--- src/xpc/execute_process_queue.ts
+++ src/xpc/execute_process_queue.ts
@@ -8,20 +8,24 @@ export class ExecuteProcessQueue extends ProcessQueue {
         return this.queue.add(async () => {
             return new Promise<string>((resolve, reject) => {
                 const child = spawn.fork(`./src/xpc/execute_process.ts`)
+                const webhookId = JSON.parse(data).webhookId
+                winston.info(`ChildProcess created for webhookId: ${webhookId}`)
                 let succeeded = false
                 child.on("message", (actionResponse) => {
                     resolve(actionResponse)
+                    winston.info(`ChildProcess was successful for webhookId: ${webhookId}`)
                     succeeded = true
                     child.kill()
                 }).on("error", (err) => {
-                    winston.warn(`ChildProcess sent error message: ${err.message}`)
+                    winston.warn(`ChildProcess sent error message: ${err.message}, for webhookId: ${webhookId}`)
                     if (!child.killed) {
                         child.kill()
                     }
                     reject(err)
                 }).on("exit", (code: number, signal: string) => {
                     if (!succeeded) {
-                        winston.warn(`ChildProcess exited with code: ${code}, signal: ${signal}`)
+                        winston.warn(
+                            `ChildProcess exited with code: ${code}, signal: ${signal}, for webhookId: ${webhookId}`)
                     }
                     if (!child.killed) {
                         child.kill()
@@ -29,7 +33,7 @@ export class ExecuteProcessQueue extends ProcessQueue {
                     reject(signal)
                 }).on("disconnect", () => {
                     if (!succeeded) {
-                        winston.info(`ChildProcess disconnected`)
+                        winston.info(`ChildProcess disconnected, for webhookId: ${webhookId}`)
                     }
                     if (!child.killed) {
                         child.kill()
@@ -37,7 +41,8 @@ export class ExecuteProcessQueue extends ProcessQueue {
                     reject("Child Disconnected")
                 }).on("close", (code: number, signal: string) => {
                     if (!succeeded) {
-                        winston.warn(`ChildProcess closed with code: ${code}, signal: ${signal}`)
+                        winston.warn(
+                            `ChildProcess closed with code: ${code}, signal: ${signal}, for webhookId: ${webhookId}`)
                     }
                     if (!child.killed) {
                         child.kill()
