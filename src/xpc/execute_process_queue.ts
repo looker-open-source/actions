b371f69dac90f0f5cc8357c36fdeabddeec45978
Phillip Peralez <pwperalez@gmail.com>
2018-09-14 12:04:16 -0700

    Added a mechanism to clear the job queue of a hung job

--- src/xpc/execute_process_queue.ts
+++ src/xpc/execute_process_queue.ts
@@ -3,6 +3,15 @@ import * as winston from "winston"
 import {ProcessQueue} from "./process_queue"
 
 export class ExecuteProcessQueue extends ProcessQueue {
+    PROCESS_TIMEOUT  = 1000 * 60 * 60 // 1 hour in milliseconds
+
+    processTimeoutKiller(child: spawn.ChildProcess, webhookId: string, cb: any) {
+        winston.warn(`Killing ChildProcess for webhookId: ${webhookId}`)
+        if (!child.killed) {
+            child.kill()
+        }
+        cb("Killed ChildProcess due to timeout in responding to parent process")
+    }
 
     async run(data: string) {
         return this.queue.add(async () => {
@@ -11,18 +20,22 @@ export class ExecuteProcessQueue extends ProcessQueue {
                 const webhookId = JSON.parse(data).webhookId
                 winston.info(`ChildProcess created for webhookId: ${webhookId}`)
                 let succeeded = false
+                const timeout = setTimeout(this.processTimeoutKiller, this.PROCESS_TIMEOUT, child, webhookId, reject)
                 child.on("message", (actionResponse) => {
+                    clearTimeout(timeout)
                     resolve(actionResponse)
                     winston.info(`ChildProcess was successful for webhookId: ${webhookId}`)
                     succeeded = true
                     child.kill()
                 }).on("error", (err) => {
+                    clearTimeout(timeout)
                     winston.warn(`ChildProcess sent error message: ${err.message}, for webhookId: ${webhookId}`)
                     if (!child.killed) {
                         child.kill()
                     }
                     reject(err)
                 }).on("exit", (code: number, signal: string) => {
+                    clearTimeout(timeout)
                     if (!succeeded) {
                         winston.warn(
                             `ChildProcess exited with code: ${code}, signal: ${signal}, for webhookId: ${webhookId}`)
@@ -32,6 +45,7 @@ export class ExecuteProcessQueue extends ProcessQueue {
                     }
                     reject(signal)
                 }).on("disconnect", () => {
+                    clearTimeout(timeout)
                     if (!succeeded) {
                         winston.info(`ChildProcess disconnected, for webhookId: ${webhookId}`)
                     }
@@ -40,6 +54,7 @@ export class ExecuteProcessQueue extends ProcessQueue {
                     }
                     reject("Child Disconnected")
                 }).on("close", (code: number, signal: string) => {
+                    clearTimeout(timeout)
                     if (!succeeded) {
                         winston.warn(
                             `ChildProcess closed with code: ${code}, signal: ${signal}, for webhookId: ${webhookId}`)
