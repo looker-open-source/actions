7db01bde1f18e5a4fa7aac117a0ba1521b22aed4
Wil Gieseler <wil@wilgieseler.com>
2018-10-05 12:42:51 -0700

    better env var name and docs

--- src/xpc/process_queue.ts
+++ src/xpc/process_queue.ts
@@ -4,8 +4,10 @@ export abstract class ProcessQueue {
     queue: Pq
 
     constructor() {
-        const concurrency = process.env.ACTION_HUB_HEAVY_PROCESS_COUNT ?
-            parseInt(process.env.ACTION_HUB_HEAVY_PROCESS_COUNT, 10) : 1
+        // Actions that haven't specified executeInOwnProcess will not
+        // be affected by this process count
+        const concurrency = process.env.ACTION_HUB_EXECUTE_PROCESS_COUNT ?
+            parseInt(process.env.ACTION_HUB_EXECUTE_PROCESS_COUNT, 10) : 1
         this.queue = new Pq({concurrency})
     }
 
