77746c83767fee846360dabb1400dfdd8be59018
Pascal <pascal@4mile.io>
2018-10-05 22:46:42 -0400

    trying queue

--- src/actions/marketo/marketo_transaction.ts
+++ src/actions/marketo/marketo_transaction.ts
@@ -6,6 +6,10 @@ const MARKETO: any = require("node-marketo-rest")
 
 const numLeadsAllowedPerCall = 100
 
+function log(...args: any[]) {
+  console.log(...args)
+}
+
 function logJson(label: string, object: any) {
   console.log("\n================================")
   console.log(`${label}:\n`)
@@ -116,16 +120,23 @@ export default class MarketoTransaction {
       campaignErrors: results.reduce((memo: any[], r: Result) => memo.concat(r.campaignErrors), []),
     }
 
+    log("leads", result.leads.length)
+    log("skipped", result.skipped.length)
+    log("leadErrors", result.leadErrors.length)
+    log("campaignErrors", result.campaignErrors.length)
+
+    logJson("skipped[0]", result.skipped[0])
+
     console.timeEnd("all done")
 
-    if (this.hasErrors(result)) {
-      const message = this.getErrorMessage(result)
-      console.log("error message", message)
-      return new Hub.ActionResponse({
-        success: false,
-        message,
-      })
-    }
+    // if (this.hasErrors(result)) {
+    //   const message = this.getErrorMessage(result)
+    //   console.log("error message", message)
+    //   return new Hub.ActionResponse({
+    //     success: false,
+    //     message,
+    //   })
+    // }
 
     return new Hub.ActionResponse({ success: true })
   }
@@ -205,41 +216,41 @@ export default class MarketoTransaction {
     })
   }
 
-  private hasErrors(result: Result) {
-    return (
-      result.skipped.length
-      || result.leadErrors.length
-      || result.campaignErrors.length
-    )
-  }
+  // private hasErrors(result: Result) {
+  //   return (
+  //     result.skipped.length
+  //     || result.leadErrors.length
+  //     || result.campaignErrors.length
+  //   )
+  // }
 
-  private getErrorMessage(result: Result) {
-    const condensed: any = {}
-    if (result.skipped.length) {
-      condensed.skipped = this.getSkippedReasons(result.skipped)
-    }
-    if (result.leadErrors.length) {
-      condensed.leadErrors = result.leadErrors
-    }
-    if (result.campaignErrors.length) {
-      condensed.campaignErrors = result.campaignErrors
-    }
-    return JSON.stringify(condensed)
-  }
+  // private getErrorMessage(result: Result) {
+  //   const condensed: any = {}
+  //   if (result.skipped.length) {
+  //     condensed.skipped = this.getSkippedReasons(result.skipped)
+  //   }
+  //   if (result.leadErrors.length) {
+  //     condensed.leadErrors = result.leadErrors
+  //   }
+  //   if (result.campaignErrors.length) {
+  //     condensed.campaignErrors = result.campaignErrors
+  //   }
+  //   return JSON.stringify(condensed)
+  // }
 
-  private getSkippedReasons(skipped: any[]) {
-    const reasons: any = {}
+  // private getSkippedReasons(skipped: any[]) {
+  //   const reasons: any = {}
 
-    skipped.forEach((item: any) => {
-      // get the reason item was skipped
-      const reason = item.result.reasons[0].message
-      // get the list of emails with that same reason
-      // or create the list if this is the first one
-      const list = reasons[reason] || (reasons[reason] = [])
-      list.push(item.email)
-    })
+  //   skipped.forEach((item: any) => {
+  //     // get the reason item was skipped
+  //     const reason = item.result.reasons[0].message
+  //     // get the list of emails with that same reason
+  //     // or create the list if this is the first one
+  //     const list = reasons[reason] || (reasons[reason] = [])
+  //     list.push(item.email)
+  //   })
 
-    return reasons
-  }
+  //   return reasons
+  // }
 
 }
