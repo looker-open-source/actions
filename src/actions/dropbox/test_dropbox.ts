fb7e4d3d0615b6bd16c9a8c328f73e7c23eb2aeb
lizardtotem <lizardtotem@gmail.com>
2019-03-11 13:22:45 -0700

    fix test for dropbox app key

--- src/actions/dropbox/test_dropbox.ts
+++ src/actions/dropbox/test_dropbox.ts
@@ -162,10 +162,11 @@ describe(`${action.constructor.name} unit tests`, () => {
 
   describe("oauth", () => {
     it("returns correct redirect url", () => {
+      process.env.DROPBOX_ACTION_APP_KEY = "testingkey"
       const prom = action.oauthUrl("https://actionhub.com/actions/dropbox/oauth_redirect",
         `eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0ZS9hc2RmYXNkZmFzZGZhc2RmIn0`)
       return chai.expect(prom).to.eventually.equal("https://www.dropbox.com/oauth2/authorize?response_type=code&" +
-        "client_id=ciauyqq5mylk35p&redirect_uri=https%3A%2F%2Factionhub.com%2Factions%2Fdropbox%2Foauth_redirect&" +
+        "client_id=testingkey&redirect_uri=https%3A%2F%2Factionhub.com%2Factions%2Fdropbox%2Foauth_redirect&" +
         "force_reapprove=true&" +
         "state=eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0ZS9hc2RmYXNkZmFzZGZhc2RmIn0")
     })
