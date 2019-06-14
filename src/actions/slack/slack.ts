6d8e6c08a0f47402bd19de00c78c0927d075414e
Jamie Davidson <jdavidson@gmail.com>
2019-06-14 08:35:32 -0700

    remove unneeded file

--- src/actions/slack/slack.ts
+++ src/actions/slack/slack.ts
@@ -56,26 +56,26 @@ https://github.com/looker/actions/blob/master/src/actions/slack/README.md`,
     const form = new Hub.ActionForm()
 
     try {
-    const slack = this.slackClientFromRequest(request)
-    const slackUtility = new WebClientUtilities(slack)
-    const channels = await slackUtility.usableChannels()
+      const slack = this.slackClientFromRequest(request)
+      const slackUtility = new WebClientUtilities(slack)
+      const channels = await slackUtility.usableChannels()
 
-    form.fields = [{
-      description: "Name of the Slack channel you would like to post to.",
-      label: "Share In",
-      name: "channel",
-      options: channels.map((channel) => ({ name: channel.id, label: channel.label })),
-      required: true,
-      type: "select",
-    }, {
-      label: "Comment",
-      type: "string",
-      name: "initial_comment",
-    }, {
-      label: "Filename",
-      name: "filename",
-      type: "string",
-    }]
+      form.fields = [{
+        description: "Name of the Slack channel you would like to post to.",
+        label: "Share In",
+        name: "channel",
+        options: channels.map((channel) => ({ name: channel.id, label: channel.label })),
+        required: true,
+        type: "select",
+      }, {
+        label: "Comment",
+        type: "string",
+        name: "initial_comment",
+      }, {
+        label: "Filename",
+        name: "filename",
+        type: "string",
+      }]
 
     } catch (e) {
       form.error = prettySlackError(e)
