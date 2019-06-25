d49a3461829450dbf957f8f9fc9ecadc2d7de8e5
Jamie Davidson <jdavidson@gmail.com>
2019-06-25 12:20:57 -0700

    integrate both actions and intelligently use oauth when no token is provided

--- src/actions/slack/slack.ts
+++ src/actions/slack/slack.ts
@@ -1,9 +1,28 @@
 import * as Hub from "../../hub"
 
-import { WebClient } from "@slack/client"
-import { prettySlackError, WebClientUtilities } from "./webclient_utilities"
+import * as querystring from "querystring"
+import * as https from "request-promise-native"
+import {URL} from "url"
+import * as winston from "winston"
 
-export class SlackAttachmentAction extends Hub.Action {
+import { WebClient } from "@slack/client"
+
+const apiLimitSize = 1000
+
+interface Channel {
+  id: string,
+  label: string,
+}
+
+function prettySlackError(e: any) {
+  if (e.message === "An API error occurred: invalid_auth") {
+    return "Your Slack authentication credentials are not valid."
+  } else {
+    return e
+  }
+}
+
+export class SlackAttachmentAction extends Hub.OAuthAction {
 
   name = "slack"
   label = "Slack Attachment"
@@ -14,7 +33,7 @@ export class SlackAttachmentAction extends Hub.Action {
   params = [{
     name: "slack_api_token",
     label: "Slack API Token",
-    required: true,
+    required: false,
     description: `A Slack API token that includes the permissions "channels:read", \
 "users:read", and "files:write:user". You can follow the instructions to get a token at \
 https://github.com/looker/actions/blob/master/src/actions/slack/README.md`,
@@ -42,18 +61,19 @@ https://github.com/looker/actions/blob/master/src/actions/slack/README.md`,
         })
       })
     } catch (e) {
-      response = new Hub.ActionResponse({success: false, message: e.message })
+      response = new Hub.ActionResponse({success: false, message: e.message})
+      if (!request.params.slack_api_token) {
+        response.state = new Hub.ActionState()
+        response.state.data = "reset"
+      }
     }
     return response
   }
 
   async form(request: Hub.ActionRequest) {
     const form = new Hub.ActionForm()
-
     try {
-      const slack = this.slackClientFromRequest(request)
-      const slackUtility = new WebClientUtilities(slack)
-      const channels = await slackUtility.usableChannels()
+      const channels = await this.usableChannels(request)
 
       form.fields = [{
         description: "Name of the Slack channel you would like to post to.",
@@ -71,18 +91,168 @@ https://github.com/looker/actions/blob/master/src/actions/slack/README.md`,
         name: "filename",
         type: "string",
       }]
-
     } catch (e) {
-      form.error = prettySlackError(e)
+      if (request.params.slack_api_token) {
+        form.error = prettySlackError(e)
+      } else {
+        const actionCrypto = new Hub.ActionCrypto()
+        const jsonString = JSON.stringify({stateurl: request.params.state_url})
+        const ciphertextBlob = await actionCrypto.encrypt(jsonString).catch((err: string) => {
+          winston.error("Encryption not correctly configured")
+          throw err
+        })
+        form.state = new Hub.ActionState()
+        form.state.data = "reset"
+        form.fields.push({
+          name: "login",
+          type: "oauth_link",
+          label: "Log in",
+          description: "In order to send to a file, you will need to log in" +
+            " once to your Slack account.",
+          oauth_url: `${process.env.ACTION_HUB_BASE_URL}/actions/slack/oauth?state=${ciphertextBlob}`,
+        })
+      }
     }
-
     return form
   }
 
-  slackClientFromRequest(request: Hub.ActionRequest) {
-    return new WebClient(request.params.slack_api_token!)
+  async usableChannels(request: Hub.ActionRequest) {
+    let channels = await this.usablePublicChannels(request)
+    channels = channels.concat(await this.usableDMs(request))
+    channels.sort((a, b) => ((a.label < b.label) ? -1 : 1 ))
+    return channels
   }
 
+  async usablePublicChannels(request: Hub.ActionRequest) {
+    const slack = this.slackClientFromRequest(request)
+    const options: any = {
+      exclude_archived: true,
+      exclude_members: true,
+      limit: apiLimitSize,
+    }
+    const pageLoaded = async (accumulatedChannels: any[], response: any): Promise<any[]> => {
+      const mergedChannels = accumulatedChannels.concat(response.channels)
+
+      // When a `next_cursor` exists, recursively call this function to get the next page.
+      if (response.response_metadata &&
+          response.response_metadata.next_cursor &&
+          response.response_metadata.next_cursor !== "") {
+        const pageOptions = { ...options }
+        pageOptions.cursor = response.response_metadata.next_cursor
+        return pageLoaded(mergedChannels, await slack.channels.list(pageOptions))
+      }
+      return mergedChannels
+    }
+    const paginatedChannels = await pageLoaded([], await slack.channels.list(options))
+    const channels = paginatedChannels.filter((c: any) => c.is_member && !c.is_archived)
+    const reformatted: Channel[] = channels.map((channel: any) => ({id: channel.id, label: `#${channel.name}`}))
+    return reformatted
+  }
+
+  async usableDMs(request: Hub.ActionRequest) {
+    const slack = this.slackClientFromRequest(request)
+    const options: any = {
+      limit: apiLimitSize,
+    }
+    const pageLoaded = async (accumulatedUsers: any[], response: any): Promise<any[]> => {
+      const mergedUsers = accumulatedUsers.concat(response.members)
+
+      // When a `next_cursor` exists, recursively call this function to get the next page.
+      if (response.response_metadata &&
+          response.response_metadata.next_cursor &&
+          response.response_metadata.next_cursor !== "") {
+        const pageOptions = { ...options }
+        pageOptions.cursor = response.response_metadata.next_cursor
+        return pageLoaded(mergedUsers, await slack.users.list(pageOptions))
+      }
+      return mergedUsers
+    }
+    const paginatedUsers = await pageLoaded([], await slack.users.list(options))
+    const users = paginatedUsers.filter((u: any) => {
+      return !u.is_restricted && !u.is_ultra_restricted && !u.is_bot && !u.deleted
+    })
+    const reformatted: Channel[] = users.map((user: any) => ({id: user.id, label: `@${user.name}`}))
+    return reformatted
+  }
+
+  async oauthUrl(redirectUri: string, encryptedState: string) {
+    const url = new URL("https://slack.com/oauth/authorize")
+    url.search = querystring.stringify({
+      client_id: process.env.SLACK_CLIENT,
+      redirect_uri: redirectUri,
+      state: encryptedState,
+      scope: "channels:read,chat:write:user,files:write:user,groups:read,users:read",
+    })
+    return url.toString()
+  }
+
+  async oauthFetchInfo(urlParams: { [key: string]: string }, redirectUri: string) {
+    const actionCrypto = new Hub.ActionCrypto()
+    const plaintext = await actionCrypto.decrypt(urlParams.state).catch((err: string) => {
+      winston.error("Encryption not correctly configured" + err)
+      throw err
+    })
+
+    const credentials = await this.getCredentialsFromCode(urlParams.code, redirectUri)
+
+    const payload = JSON.parse(plaintext)
+    await https.post({
+      url: payload.stateurl,
+      body: JSON.stringify(credentials),
+    }).catch((_err) => { winston.error(_err.toString()) })
+  }
+
+  async oauthCheck(request: Hub.ActionRequest) {
+    if (!request.params.slack_api_token) {
+      const slack = this.slackClientFromRequest(request)
+      try {
+        await slack.channels.list()
+        return true
+      } catch (err) {
+        return false
+      }
+    } else {
+      return false
+    }
+  }
+
+  protected async getCredentialsFromCode(code: string, redirect: string, refresh = false) {
+    if (code) {
+      const response = await this.slackClient().oauth.access({
+        client_id: process.env.SLACK_CLIENT!,
+        client_secret: process.env.SLACK_SECRET!,
+        code,
+        redirect_uri: redirect,
+        grant_type: refresh ? "refresh_token" : "authorization_code",
+      })
+      return response
+    } else {
+      throw "code does not exist"
+    }
+  }
+
+  private slackClientFromRequest(request: Hub.ActionRequest) {
+    if (request.params.slack_api_token) {
+      return this.slackClient(request.params.slack_api_token)
+    } else {
+      let accessToken
+      if (request.params.state_json) {
+        try {
+          const stateJson = JSON.parse(request.params.state_json)
+          accessToken = stateJson.access_token
+        } catch {
+          winston.warn("Could not parse state_json")
+        }
+      }
+      return this.slackClient(accessToken)
+    }
+  }
+
+  private slackClient(token?: string) {
+    return new WebClient(token)
+  }
 }
 
-Hub.addAction(new SlackAttachmentAction())
+if (process.env.SLACK_CLIENT && process.env.SLACK_SECRET) {
+  Hub.addAction(new SlackAttachmentAction())
+}
--- src/actions/slack/test_slack.ts
+++ src/actions/slack/test_slack.ts
@@ -1,9 +1,12 @@
+import * as b64 from "base64-url"
 import * as chai from "chai"
 import * as sinon from "sinon"
 
 import concatStream = require("concat-stream")
 
 import * as Hub from "../../hub"
+
+import { ActionCrypto } from "../../hub"
 
 import { SlackAttachmentAction } from "./slack"
 
@@ -42,6 +45,18 @@ function expectSlackMatch(request: Hub.ActionRequest, optionsMatch: FilesUploadA
 }
 
 describe(`${action.constructor.name} unit tests`, () => {
+  let encryptStub: any
+  let decryptStub: any
+
+  beforeEach(() => {
+    encryptStub = sinon.stub(ActionCrypto.prototype, "encrypt").callsFake( async (s: string) => b64.encode(s) )
+    decryptStub = sinon.stub(ActionCrypto.prototype, "decrypt").callsFake( async (s: string) => b64.decode(s) )
+  })
+
+  afterEach(() => {
+    encryptStub.restore()
+    decryptStub.restore()
+  })
 
   describe("action", () => {
 
@@ -59,6 +74,10 @@ describe(`${action.constructor.name} unit tests`, () => {
 
     it("sends to right body, channel and filename if specified", () => {
       const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
+      request.params = {
+        slack_api_token: "token",
+      }
       request.formParams = {
         channel: "mychannel",
         filename: "mywackyfilename",
@@ -76,8 +95,70 @@ describe(`${action.constructor.name} unit tests`, () => {
       })
     })
 
+    it("sends to right body, channel and filename if specified with oauth", () => {
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
+      request.params = {
+        stateUrl: "https://looker.state.url.com/action_hub_state/asdfasdfasdfasdf",
+        stateJson: `{"access_token": "token"}`,
+      }
+      request.formParams = {
+        channel: "mychannel",
+        filename: "mywackyfilename",
+        initial_comment: "mycomment",
+      }
+      request.attachment = {
+        dataBuffer: Buffer.from("1,2,3,4", "utf8"),
+        fileExtension: "csv",
+      }
+      return expectSlackMatch(request, {
+        file: request.attachment.dataBuffer,
+        filename: request.formParams.filename,
+        channels: request.formParams.channel,
+        initial_comment: request.formParams.initial_comment,
+      })
+    })
+
+    it("sets state to reset if error in files.upload", (done) => {
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
+      request.params = {
+        stateUrl: "https://looker.state.url.com/action_hub_state/asdfasdfasdfasdf",
+        stateJson: `{"access_token": "token"}`,
+      }
+      request.formParams = {
+        channel: "mychannel",
+        filename: "mywackyfilename",
+        initial_comment: "mycomment",
+      }
+      request.attachment = {
+        dataBuffer: Buffer.from("1,2,3,4", "utf8"),
+        fileExtension: "csv",
+      }
+
+      const stubClient = sinon.stub(action as any, "slackClientFromRequest")
+        .callsFake(() => ({
+          files: {
+            upload: async () => Promise.reject({message: "error"}),
+          },
+        }))
+
+      const resp = action.validateAndExecute(request)
+      chai.expect(resp).to.eventually.deep.equal({
+        success: false,
+        state: {data: "reset"},
+        message: "error",
+        refreshQuery: false,
+        validationErrors: [],
+      }).and.notify(stubClient.restore).and.notify(done)
+    })
+
     it("sends right body and channel", () => {
       const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
+      request.params = {
+        slack_api_token: "token",
+      }
       request.formParams = {
         channel: "mychannel",
         initial_comment: "mycomment",
@@ -96,6 +177,10 @@ describe(`${action.constructor.name} unit tests`, () => {
 
     it("returns failure on slack files.upload error", () => {
       const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
+      request.params = {
+        slack_api_token: "token",
+      }
       request.formParams = {
         channel: "mychannel",
         initial_comment: "mycomment",
@@ -129,6 +214,63 @@ describe(`${action.constructor.name} unit tests`, () => {
 
   })
 
+  function expectSlackFormMatch(request: Hub.ActionRequest, match: any, done: MochaDone) {
+    const stubClient = sinon.stub(action as any, "slackClientFromRequest")
+    .callsFake(() => ({
+      channels: {
+        list: (filters: any) => {
+          if (filters.cursor) {
+            return {
+              ok: true,
+              channels: [
+                {id: "3", name: "C", is_member: true},
+                {id: "4", name: "D", is_member: true},
+              ],
+            }
+          } else {
+            return {
+              ok: true,
+              channels: [
+                {id: "1", name: "A", is_member: true},
+                {id: "2", name: "B", is_member: true},
+              ],
+              response_metadata: {
+                next_cursor: "cursor",
+              },
+            }
+          }
+        },
+      },
+      users: {
+        list: (filters: any) => {
+          if (filters.cursor) {
+            return {
+              ok: true,
+              members: [
+                {id: "30", name: "W"},
+                {id: "40", name: "X"},
+              ],
+            }
+          } else {
+            return {
+              ok: true,
+                members: [
+                  {id: "10", name: "Z"},
+                  {id: "20", name: "Y"},
+                ],
+              response_metadata: {
+                next_cursor: "cursor",
+              },
+            }
+          }
+        },
+      },
+    }))
+
+    const form = action.validateAndFetchForm(request)
+    chai.expect(form).to.eventually.deep.equal(match).and.notify(stubClient.restore).and.notify(done)
+  }
+
   describe("form", () => {
 
     it("has form", () => {
@@ -136,63 +278,12 @@ describe(`${action.constructor.name} unit tests`, () => {
     })
 
     it("has form with correct channels", (done) => {
-      const stubClient = sinon.stub(action as any, "slackClientFromRequest")
-        .callsFake(() => ({
-          channels: {
-            list: (filters: any) => {
-              if (filters.cursor) {
-                return {
-                  ok: true,
-                  channels: [
-                    {id: "3", name: "C", is_member: true},
-                    {id: "4", name: "D", is_member: true},
-                  ],
-                }
-              } else {
-                return {
-                  ok: true,
-                  channels: [
-                    {id: "1", name: "A", is_member: true},
-                    {id: "2", name: "B", is_member: true},
-                  ],
-                  response_metadata: {
-                    next_cursor: "cursor",
-                  },
-                }
-              }
-            },
-          },
-          users: {
-            list: (filters: any) => {
-              if (filters.cursor) {
-                return {
-                  ok: true,
-                  members: [
-                    {id: "30", name: "W"},
-                    {id: "40", name: "X"},
-                  ],
-                }
-              } else {
-                return {
-                  ok: true,
-                    members: [
-                      {id: "10", name: "Z"},
-                      {id: "20", name: "Y"},
-                    ],
-                  response_metadata: {
-                    next_cursor: "cursor",
-                  },
-                }
-              }
-            },
-          },
-        }))
       const request = new Hub.ActionRequest()
       request.params = {
         slack_api_token: "foo",
       }
-      const form = action.validateAndFetchForm(request)
-      chai.expect(form).to.eventually.deep.equal({
+
+      return expectSlackFormMatch(request, {
         fields: [{
           description: "Name of the Slack channel you would like to post to.",
           label: "Share In",
@@ -218,9 +309,138 @@ describe(`${action.constructor.name} unit tests`, () => {
           name: "filename",
           type: "string",
         }],
+      }, done)
+    })
+
+    it("returns an oauth form on bad login", (done) => {
+      const stubClient = sinon.stub(action as any, "slackClientFromRequest")
+        .callsFake(() => ({
+          channels: {
+            list: async () => Promise.reject("haha I failed auth"),
+          },
+          users: {
+            list: async () => Promise.reject("haha I failed auth"),
+          },
+        }))
+      const request = new Hub.ActionRequest()
+      request.params = {
+        state_url: "https://looker.state.url.com/action_hub_state/asdfasdfasdfasdf",
+        state_json: `{"access_token": "token"}`,
+      }
+      const form = action.validateAndFetchForm(request)
+      chai.expect(form).to.eventually.deep.equal({
+        fields: [{
+          name: "login",
+          type: "oauth_link",
+          description: "In order to send to a file, you will need to log in" +
+            " once to your Slack account.",
+          label: "Log in",
+          oauth_url: `${process.env.ACTION_HUB_BASE_URL}/actions/slack/oauth?` +
+          `state=eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9` +
+          `va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0ZS9hc2RmYXNkZmFzZGZhc2RmIn0`,
+        }],
+        state: {
+          data: "reset",
+        },
       }).and.notify(stubClient.restore).and.notify(done)
     })
 
+    it("does not blow up on bad state JSON and returns an OAUTH form", (done) => {
+      const stubClient = sinon.stub(action as any, "slackClientFromRequest")
+        .callsFake(() => ({
+          channels: {
+            list: async () => Promise.reject("haha I failed auth"),
+          },
+          users: {
+            list: async () => Promise.reject("haha I failed auth"),
+          },
+        }))
+      const request = new Hub.ActionRequest()
+      request.params = {
+        state_url: "https://looker.state.url.com/action_hub_state/asdfasdfasdfasdf",
+        state_json: `{"bad": "token"}`,
+      }
+      const form = action.validateAndFetchForm(request)
+      chai.expect(form).to.eventually.deep.equal({
+        fields: [{
+          name: "login",
+          type: "oauth_link",
+          description: "In order to send to a file, you will need to log in" +
+            " once to your Slack account.",
+          label: "Log in",
+          oauth_url: `${process.env.ACTION_HUB_BASE_URL}/actions/slack/oauth?` +
+            `state=eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9` +
+            `va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0ZS9hc2RmYXNkZmFzZGZhc2RmIn0`,
+        }],
+        state: {
+          data: "reset",
+        },
+      }).and.notify(stubClient.restore).and.notify(done)
+    })
+
+    it("returns correct fields on oauth success", (done) => {
+      const request = new Hub.ActionRequest()
+      request.params = {
+        stateUrl: "https://looker.state.url.com/action_hub_state/asdfasdfasdfasdf",
+        stateJson: `{"access_token": "token"}`,
+      }
+      expectSlackFormMatch(request, {
+        fields: [{
+          description: "Name of the Slack channel you would like to post to.",
+          label: "Share In",
+          name: "channel",
+          options: [
+            {name: "1", label: "#A"},
+            {name: "2", label: "#B"},
+            {name: "3", label: "#C"},
+            {name: "4", label: "#D"},
+            {name: "30", label: "@W"},
+            {name: "40", label: "@X"},
+            {name: "20", label: "@Y"},
+            {name: "10", label: "@Z"},
+          ],
+          required: true,
+          type: "select",
+        }, {
+          label: "Comment",
+          type: "string",
+          name: "initial_comment",
+        }, {
+          label: "Filename",
+          name: "filename",
+          type: "string",
+        }],
+      }, done)
+    })
   })
 
+  describe("oauth", () => {
+    it("returns correct redirect url", () => {
+      process.env.SLACK_CLIENT = "testingclient"
+      const prom = action.oauthUrl("https://actionhub.com/actions/slack/oauth_redirect",
+        `eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0ZS9hc2RmYXNkZmFzZGZhc2RmIn0`)
+      return chai.expect(prom).to.eventually.equal(
+        "https://slack.com/oauth/authorize?" +
+        "client_id=testingclient&" +
+        "redirect_uri=https%3A%2F%2Factionhub.com%2Factions%2Fslack%2Foauth_redirect&" +
+        "state=eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0" +
+        "ZS9hc2RmYXNkZmFzZGZhc2RmIn0&" +
+        "scope=channels%3Aread%2Cchat%3Awrite%3Auser%2Cfiles%3Awrite%3Auser%2Cgroups%3Aread%2Cusers%3Aread")
+    })
+
+    it("correctly handles redirect from authorization server", (done) => {
+      const stubReq = sinon.stub(action as any, "slackClient")
+        .callsFake(() => ({
+          oauth: {
+            access: async () => Promise.resolve({access_token: "token"}),
+          },
+        }))
+      const result = action.oauthFetchInfo({code: "code",
+        state: `eyJzdGF0ZXVybCI6Imh0dHBzOi8vbG9va2VyLnN0YXRlLnVybC5jb20vYWN0aW9uX2h1Yl9zdGF0ZS9hc2RmYXNkZmFzZGZh` +
+          `c2RmIiwiYXBwIjoibXlrZXkifQ`},
+        "redirect")
+      chai.expect(result)
+        .and.notify(stubReq.restore).and.notify(done)
+    })
+  })
 })
