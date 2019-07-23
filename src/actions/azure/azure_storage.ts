7abfb802231c323d6cc5c66562b33eff1c0bcab3
Jamie Davidson <jdavidson@gmail.com>
2019-07-23 07:30:29 -0700

    Update to azure storage action to enable streaming (#248)
    Adding streaming to Azure storage action
    Fixes bug when Azure container list is empty crashing AxtionHub

--- src/actions/azure/azure_storage.ts
+++ src/actions/azure/azure_storage.ts
@@ -9,6 +9,7 @@ export class AzureStorageAction extends Hub.Action {
   iconName = "azure/azure_storage.png"
   description = "Write data files to an Azure container."
   supportedActionTypes = [Hub.ActionType.Query, Hub.ActionType.Dashboard]
+  usesStreaming = true
   requiredFields = []
   params = [
     {
@@ -27,64 +28,67 @@ export class AzureStorageAction extends Hub.Action {
   ]
 
   async execute(request: Hub.ActionRequest) {
-    if (!request.attachment || !request.attachment.dataBuffer) {
-      throw "Couldn't get data from attachment"
-    }
 
     if (!request.formParams.container) {
       throw "Need Azure container."
     }
 
-    const blobService = this.azureClientFromRequest(request)
     const fileName = request.formParams.filename || request.suggestedFilename()
     const container = request.formParams.container
-    const data = request.attachment.dataBuffer
 
     if (!fileName) {
       return new Hub.ActionResponse({ success: false, message: "Cannot determine a filename." })
     }
 
-    return new Promise<Hub.ActionResponse>((resolve, reject) => {
-      blobService.createBlockBlobFromText(
-        container,
-        fileName,
-        data,
-        (e?: Error) => {
-          if (e) {
-            reject(new Hub.ActionResponse({success: false, message: e.message}))
-          } else {
-            resolve(new Hub.ActionResponse({success: true}))
-          }
+    const blobService = this.azureClientFromRequest(request)
+    const writeStream = blobService.createWriteStreamToBlockBlob(container, fileName)
+
+    try {
+      await request.stream(async (readable) => {
+        return new Promise<any>((resolve, reject) => {
+          readable.pipe(writeStream)
+            .on("error", reject)
+            .on("finish", resolve)
         })
-    })
+      })
+      return new Hub.ActionResponse({ success: true })
+    } catch (e) {
+      return new Hub.ActionResponse({success: false, message: e.message})
+    }
   }
 
   async form(request: Hub.ActionRequest) {
     // error in type definition for listContainersSegmented currentToken?
     // https://github.com/Azure/azure-storage-node/issues/352
     const form = new Hub.ActionForm()
-    const blogService: any = this.azureClientFromRequest(request)
+    const blobService: any = this.azureClientFromRequest(request)
     return new Promise<Hub.ActionForm>((resolve, _reject) => {
-      blogService.listContainersSegmented(null, (err: any, res: any) => {
+      blobService.listContainersSegmented(null, (err: any, res: any) => {
         if (err) {
           form.error = err
           resolve(form)
         } else {
-          form.fields = [{
-            label: "Container",
-            name: "container",
-            required: true,
-            options: res.entries.map((c: any) => {
-              return {name: c.name, label: c.name}
-            }),
-            type: "select",
-            default: res.entries[0].name,
-          }, {
-            label: "Filename",
-            name: "filename",
-            type: "string",
-          }]
-          resolve(form)
+          const entries: any[] = res.entries
+          if (entries.length > 0) {
+            form.fields = [{
+              label: "Container",
+              name: "container",
+              required: true,
+              options: entries.map((c: any) => {
+                return {name: c.name, label: c.name}
+              }),
+              type: "select",
+              default: entries[0].name,
+            }, {
+              label: "Filename",
+              name: "filename",
+              type: "string",
+            }]
+            resolve(form)
+          } else {
+            form.error = "Create a container in your Azure account."
+            resolve(form)
+          }
         }
       })
     })
