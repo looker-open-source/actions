8e1479ca80138f510cad45085f2679b50b37c39d
“zzhaobraze” <Zeyuan.zhao@braze.com>
2019-09-25 11:53:09 -0400

    Fix Braze Label

--- src/actions/braze/braze.ts
+++ src/actions/braze/braze.ts
@@ -38,8 +38,8 @@ function isEmpty(obj: any) {
 }
 
 export class BrazeAction extends Hub.Action {
-  name = "brazetest"
-  label = "Braze Test"
+  name = "braze"
+  label = "Braze"
   description = "Flags users from Looker to Braze via the REST API for use with segmentation. "
     + "Ensure there's a '" + BrazeConfig.BRAZE_ID_TAG + "' field tagged in the results."
     + " MAX EXPORT: " + BrazeConfig.MAX_EXPORT + "."
