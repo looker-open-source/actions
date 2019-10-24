a4d20c0a0e2850e289f8ae0123826a2c67e7af38
JJ Bruno <brujj77@gmail.com>
2019-10-24 12:11:16 -0700

    Mparticle custom action (#275)
    Adds MParticle ML action to ActionHub

---
+++ src/actions/mparticle/mparticle_constants.ts
@@ -0,0 +1,6 @@
+export const MP_API_URL = "https://s2s.mparticle.com/v2/bulkevents/"
+export const EVENT_TYPE = "custom_event"
+export const DEV_ENVIRONMENT = "development"
+export const PROD_ENVIRONMENT = "production"
+export const USER = "user_data"
+export const EVENT = "event_data"
