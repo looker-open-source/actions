15f8300261f1decf2741af2c138675e82b3ecd5b
phillipperalez <pwperalez@gmail.com>
2019-10-29 10:30:50 -0600

    Improved extended execute (#289)
    Improves the extended process queue. This allows a action process to exist longer than its connection to Looker. This frees up the other process queues from being held up by very long actions that may just be ensuring jobs are getting done even after the data is uploaded

--- src/xpc/extended_process_queue.ts
+++ src/xpc/extended_process_queue.ts
@@ -4,66 +4,106 @@ import {ProcessQueue} from "./process_queue"
 
 export class ExtendedProcessQueue extends ProcessQueue {
   PROCESS_TIMEOUT = 1000 * 60 * 120 // 2 hours in milliseconds
+  DONE_MESSAGE = "PROCESS FINISHED"
+  MAX_EXTENDED_CHILD_CONCURRENCY = 2
+  childCounter = 0
 
-  processTimeoutKiller(child: spawn.ChildProcess, webhookId: string, cb: any) {
+  processTimeoutKiller(child: spawn.ChildProcess, webhookId: string, lookerReponseCallback: any, processCallback: any) {
     const msg = "Killed execute process due to timeout in responding to parent process"
     winston.warn(msg, {webhookId})
     if (!child.killed) {
       child.kill()
     }
-    cb(msg)
+    lookerReponseCallback(msg)
+    processCallback()
+  }
+
+  async child_runner(child: spawn.ChildProcess,
+                     data: string,
+                     webhookId: string,
+                     lookerResponseResolve: (value?: string | PromiseLike<string> | undefined) => void,
+                     lookerResponseReject: (reason?: any) => void) {
+    return new Promise<void>((processResolve, processReject) => {
+      const timeout = setTimeout(
+        this.processTimeoutKiller,
+        this.PROCESS_TIMEOUT,
+        child,
+        webhookId,
+        lookerResponseReject,
+        processReject,
+      )
+      let succeeded = false
+      child.on("message", (response) => {
+        if (response !== this.DONE_MESSAGE) {
+          lookerResponseResolve(response)
+          winston.info(`execute process returning successful response to Looker`, {webhookId})
+        } else {
+          winston.info(`execute process finished successfully`, {webhookId})
+          succeeded = true
+          clearTimeout(timeout)
+          child.kill()
+          processResolve()
+        }
+      }).on("error", (err) => {
+        clearTimeout(timeout)
+        winston.warn(`execute process sent error message`, {webhookId, message: err.message})
+        if (!child.killed) {
+          child.kill()
+        }
+        lookerResponseReject(err)
+        processReject()
+      }).on("exit", (code: number, signal: string) => {
+        clearTimeout(timeout)
+        if (!succeeded) {
+          winston.warn(`execute process exited`, {webhookId, code, signal})
+        }
+        if (!child.killed) {
+          child.kill()
+        }
+        lookerResponseReject(signal)
+        processReject()
+      }).on("disconnect", () => {
+        clearTimeout(timeout)
+        if (!succeeded) {
+          winston.info(`execute process disconnected`, {webhookId})
+        }
+        if (!child.killed) {
+          child.kill()
+        }
+        lookerResponseReject("Child Disconnected")
+        processReject()
+      }).on("close", (code: number, signal: string) => {
+        clearTimeout(timeout)
+        if (!succeeded) {
+          winston.warn(`execute process closed`, {webhookId, code, signal})
+        }
+        if (!child.killed) {
+          child.kill()
+        }
+        lookerResponseReject(signal)
+        processReject()
+      })
+      child.send(data)
+    })
   }
 
   async run(data: string) {
     return this.queue.add(async () => {
+      while (this.childCounter >= this.MAX_EXTENDED_CHILD_CONCURRENCY) {
+        await new Promise<void>((resolve) => {
+          setTimeout(resolve, 1000 * 10)
+        })
+      }
+      this.childCounter += 1
       return new Promise<string>((resolve, reject) => {
         const child = spawn.fork(`./src/xpc/execute_process.ts`)
         const webhookId = JSON.parse(data).webhookId
         winston.info(`execute process created`, {webhookId})
-        let succeeded = false
-        const timeout = setTimeout(this.processTimeoutKiller, this.PROCESS_TIMEOUT, child, webhookId, reject)
-        child.on("message", (actionResponse) => {
-          clearTimeout(timeout)
-          resolve(actionResponse)
-          winston.info(`execute process was successful`, {webhookId})
-          succeeded = true
-          child.kill()
-        }).on("error", (err) => {
-          clearTimeout(timeout)
-          winston.warn(`execute process sent error message`, {webhookId, message: err.message})
-          if (!child.killed) {
-            child.kill()
-          }
-          reject(err)
-        }).on("exit", (code: number, signal: string) => {
-          clearTimeout(timeout)
-          if (!succeeded) {
-            winston.warn(`execute process exited`, {webhookId, code, signal})
-          }
-          if (!child.killed) {
-            child.kill()
-          }
-          reject(signal)
-        }).on("disconnect", () => {
-          clearTimeout(timeout)
-          if (!succeeded) {
-            winston.info(`execute process disconnected`, {webhookId})
-          }
-          if (!child.killed) {
-            child.kill()
-          }
-          reject("Child Disconnected")
-        }).on("close", (code: number, signal: string) => {
-          clearTimeout(timeout)
-          if (!succeeded) {
-            winston.warn(`execute process closed`, {webhookId, code, signal})
-          }
-          if (!child.killed) {
-            child.kill()
-          }
-          reject(signal)
+        this.child_runner(child, data, webhookId, resolve, reject).then(() => {
+          this.childCounter -= 1
+        }).catch(() => {
+          this.childCounter -= 1
         })
-        child.send(data)
       })
     })
   }
