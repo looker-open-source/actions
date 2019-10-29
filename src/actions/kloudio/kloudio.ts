607a695bfc53e0262a1e3bf4fd31f38edf64bcd1
Aniruddh Mathur <aniruddh@Aniruddhs-Air.attlocal.net>
2019-10-28 23:46:01 -0700

    correcting data object parsing code

--- src/actions/kloudio/kloudio.ts
+++ src/actions/kloudio/kloudio.ts
@@ -100,7 +100,7 @@ export class KloudioAction extends Hub.Action {
     const labelIds = request.attachment.dataJSON.fields.dimensions.map((labelId: { name: any; }) => labelId.name)
     winston.info(labelIds[0])
     const dataRows = parseData(request.attachment.dataJSON.data, labelIds)
-    winston.info("length of row data is " + JSON.stringify(dataRows))
+    winston.info("all of row data is " + dataRows)
     //
     AWS.config.update({ accessKeyId: request.params.aws_access_key, secretAccessKey: request.params.aws_secret_key })
     const s3Response = await uploadToS3("s3_filename", request.attachment.dataJSON, newBucket, newAwsKey,
@@ -172,7 +172,7 @@ async function uploadToS3(file: string, data: any, bucket: any, awsKey: any, aws
 }
 
 function parseData(data: any, labels: any) {
-    const row = ["he"]
+    const rowA = []
     // const dataN = JSON.parse(data)
     const dataLen = data.length
     const rowL = labels.length
@@ -180,15 +180,19 @@ function parseData(data: any, labels: any) {
     winston.info("length of row is " +  rowL)
     winston.info("data after parsing is" + data)
     // tslint:disable-next-line: forin
-    /*
-    for (let i = 0; i < 5; i++) {
-        if (dataN[i]) {
-            winston.info(dataN[i])
-            row.push(dataN[i])
+    for (const row of data) {
+        const tempA = []
+        for (const label of labels) {
+            if (row[label].rendered) {
+                tempA.push(row[label].rendered)
+            } else {
+                tempA.push(row[label].value)
+            }
         }
-    }*/
+        rowA.push(tempA)
+    }
 
-    return row
+    return rowA
 }
 
 Hub.addAction(new KloudioAction())
