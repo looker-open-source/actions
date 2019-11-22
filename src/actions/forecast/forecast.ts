4d7711f702f4109d1f762730bed53eb3a91f7b9b
Eric Foard <eric@4mile.io>
2019-11-22 14:07:01 -0800

    add AWSForecast params property, exposing action settings

--- src/actions/forecast/forecast.ts
+++ src/actions/forecast/forecast.ts
@@ -1,3 +1,4 @@
+import * as winston from "winston"
 import * as Hub from "../../hub"
 
 export class ForecastAction extends Hub.Action {
@@ -5,7 +6,82 @@ export class ForecastAction extends Hub.Action {
   name = "amazon_forecast"
   label = "Amazon Forecast"
   supportedActionTypes = [Hub.ActionType.Query]
-  params = []
+  params = [
+    {
+      name: "accessKeyId",
+      label: "Access Key",
+      required: true,
+      sensitive: true,
+      description: "Your AWS access key ID.",
+    },
+    {
+      name: "secretAccessKey",
+      label: "Secret Key",
+      required: true,
+      sensitive: true,
+      description: "Your AWS secret access key.",
+    },
+    {
+      name: "roleArn",
+      label: "Role ARN",
+      required: true,
+      sensitive: false,
+      description: "ARN of role allowing Forecast to read from your S3 bucket(s)",
+    },
+    {
+      name: "region",
+      label: "Region",
+      required: true,
+      sensitive: false,
+      description: "AWS Region where you run Forecast",
+    },
+    {
+      name: "user_email",
+      label: "Looker User Email",
+      required: true,
+      description: `
+        Click the button on the right and select 'Email'.
+        This is required for the action to send status emails
+        when action is complete.
+      `,
+      sensitive: false,
+    },
+    {
+      name: "smtpHost",
+      label: "SMTP Host",
+      required: true,
+      sensitive: false,
+      description: "Host for sending emails.",
+    },
+    {
+      name: "smtpPort",
+      label: "SMTP Port",
+      required: true,
+      sensitive: false,
+      description: "Port for sending emails.",
+    },
+    {
+      name: "smtpFrom",
+      label: "SMTP From",
+      required: true,
+      sensitive: false,
+      description: "From for sending emails.",
+    },
+    {
+      name: "smtpUser",
+      label: "SMTP User",
+      required: true,
+      sensitive: false,
+      description: "User for sending emails.",
+    },
+    {
+      name: "smtpPass",
+      label: "SMTP Pass",
+      required: true,
+      sensitive: false,
+      description: "Pass for sending emails.",
+    },
+  ]
 
   // optional fields
   description = "Import data into Amazon Forecast, train a model, and generate a forecast from that model"
@@ -19,11 +95,13 @@ export class ForecastAction extends Hub.Action {
 
   async execute(request: Hub.ActionRequest) {
     // TODO
+    winston.debug(JSON.stringify(request.params, null, 2))
     return new Hub.ActionResponse({ success: true })
   }
 
   async form(request: Hub.ActionRequest) {
     // TODO
+    winston.debug(JSON.stringify(request.params, null, 2))
     return new Hub.ActionForm()
   }
 }
