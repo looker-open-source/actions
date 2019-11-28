c83a852390d8dccfe4138d944b8820c5536b9dd6
Aniruddh Mathur <aniruddh@Aniruddhs-MacBook-Air.local>
2019-11-27 16:08:52 -0800

    corrected the URLs

--- src/actions/kloudio/kloudio.ts
+++ src/actions/kloudio/kloudio.ts
@@ -4,8 +4,8 @@ import * as uuid from "uuid"
 import * as winston from "winston"
 import * as Hub from "../../hub"
 
-const signedUrl = "https://api-test.kloud.io/v1/tools/signed-url-put-object?key="
-const API_URL = "https://czd7a5synj.execute-api.us-west-2.amazonaws.com/test/dest/send"
+const signedUrl = "https://api.kloud.io/v1/tools/signed-url-put-object?key="
+const API_URL = "https://looker.kloud.io"
 const s3Bool = true
 let data = {}
 
