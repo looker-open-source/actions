a7ad6ea430ecd23537df896fed9d5720f3ab03f6
phillipperalez <pwperalez@gmail.com>
2020-01-24 12:35:23 -0700

    Remove custom Marketo queue and add standard priotity queue

--- src/actions/segment/segment.ts
+++ src/actions/segment/segment.ts
@@ -94,6 +94,7 @@ export class SegmentAction extends Hub.Action {
 
       await request.streamJsonDetail({
         onFields: (fields) => {
+          winston.info(JSON.stringify(fields))
           fieldset = Hub.allFields(fields)
           segmentFields = this.segmentFields(fieldset)
           this.unassignedSegmentFieldsCheck(segmentFields)
