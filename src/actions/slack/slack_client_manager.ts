e2217ecb2d6810a6b648f201409bc4d428202574
An Nguyen <ha.an.nguyen@gmail.com>
2020-01-25 11:01:29 -0800

    add slack client manager

---
+++ src/actions/slack/slack_client_manager.ts
@@ -0,0 +1,49 @@
+import {WebClient} from "@slack/client"
+import * as Hub from "../../hub"
+import * as semver from "semver"
+
+const PLACEHOLDER_WORKSPACE = "any"
+
+interface WorkspaceAwareStateJson {
+    install_id: string,
+    token: string
+}
+
+export class SlackClientManager {
+    private selectedInstallId: string | undefined;
+    private clients: { [key: string]: WebClient };
+
+    constructor(request: Hub.ActionRequest) {
+        const stateJson = request.params.state_json
+
+        if (!stateJson) {
+            this.clients = {}
+        } else if (request.lookerVersion && semver.gte(request.lookerVersion, "7.1.0")) {
+            this.selectedInstallId = request.formParams.workspace
+            this.clients = (JSON.parse(stateJson) as WorkspaceAwareStateJson[])
+                .reduce((accumulator, stateJsonItem) => {
+                    if (stateJsonItem.token) {
+                        accumulator[stateJsonItem.install_id] = this.makeClient(stateJsonItem.token)
+                    }
+                    return accumulator
+                }, {} as { [key: string]: WebClient })
+        } else {
+            this.selectedInstallId = PLACEHOLDER_WORKSPACE
+            this.clients = {[PLACEHOLDER_WORKSPACE]: this.makeClient(stateJson)}
+        }
+    }
+
+    hasAnyClients = (): boolean =>
+        this.clients && this.clients.constructor === Object && Object.entries(this.clients).length === 0
+
+    getClients = (): WebClient[] =>
+        this.clients && this.clients.constructor === Object ? Object.values(this.clients) : []
+
+    hasSelectedClient = (): boolean => !!this.selectedInstallId
+
+    getSelectedClient = (): WebClient | null => this.selectedInstallId ? this.clients[this.selectedInstallId] : null
+
+    getClient = (installId: string): WebClient | null => this.clients[installId]
+
+    private makeClient = (token: string): WebClient => new WebClient(token)
+}
