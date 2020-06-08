c2967c0a1cdc1b5c221cad6e615928db2f0ae534
arnab <arnab@rudderlabs.com>
2020-06-08 12:24:22 +0530

    Added RudderStack

---
+++ src/actions/rudderstack/rudderstack_group.ts
@@ -0,0 +1,21 @@
+import * as Hub from "../../hub"
+import { RudderAction, RudderCalls, RudderTags } from "./rudderstack"
+
+export class RudderGroupAction extends RudderAction {
+
+  tag = RudderTags.RudderGroupId
+
+  name = "rudder_group"
+  label = "Rudder Group"
+  iconName = "rudder/rudder.png"
+  description = "Add traits and / or users to your Rudder groups."
+  requiredFields = [{ tag: this.tag , any_tag: this.allowedTags}]
+  minimumSupportedLookerVersion = "5.5.0"
+
+  async execute(request: Hub.ActionRequest) {
+    return this.executeRudder(request, RudderCalls.Group)
+  }
+
+}
+
+Hub.addAction(new RudderGroupAction())
