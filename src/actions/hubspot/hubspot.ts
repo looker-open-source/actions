b2eec252d59fd865d42c55ae1661cc91f281317d
Marijn van Aerle <marijn.vanaerle@gmail.com>
2021-01-05 17:35:36 +0100

    Replace dot with underscore in property names

--- src/actions/hubspot/hubspot.ts
+++ src/actions/hubspot/hubspot.ts
@@ -181,7 +181,8 @@ export class HubspotAction extends Hub.Action {
                 fieldName !== hubspotIdFieldName &&
                 hiddenFields.indexOf(fieldName) === -1
               ) {
-                properties[fieldName] = fieldSet.value
+                const safeFieldName = fieldName.replace(".", "_");
+                properties[safeFieldName] = fieldSet.value;
               }
             })
 
