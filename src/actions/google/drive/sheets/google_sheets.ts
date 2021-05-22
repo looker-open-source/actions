67dcfff85f7b8336982a613f6211096db5e18039
Phillip Peralez <peralez@google.com>
2021-05-22 20:35:13 +0000

    Add global log to catch all execute fails in Google SHeets

--- src/actions/google/drive/sheets/google_sheets.ts
+++ src/actions/google/drive/sheets/google_sheets.ts
@@ -55,6 +55,7 @@ export class GoogleSheetsAction extends GoogleDriveAction {
                     resp.success = true
                 }
             } catch (e) {
+                winston.error(`Failed execute for Google Sheets.`, {webhookId: request.webhookId})
                 resp.success = false
                 resp.message = e.message
             }
