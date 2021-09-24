ce318d56f77b3f9c429c2a8ce6dd0c1237180964
David Szajngarten <davidtamaki@google.com>
2021-09-24 17:27:54 +0100

    formating typescript for tslint

--- src/actions/salesforce/campaigns/campaigns_send_data.ts
+++ src/actions/salesforce/campaigns/campaigns_send_data.ts
@@ -1,77 +1,79 @@
-import { Tokens, CHUNK_SIZE } from "../campaigns/salesforce_campaigns";
-import { sfdcConnFromRequest } from "../common/oauth_helper";
-import * as Hub from "../../../hub";
-import * as jsforce from "jsforce";
+import * as jsforce from "jsforce"
+import * as Hub from "../../../hub"
+import { CHUNK_SIZE, Tokens } from "../campaigns/salesforce_campaigns"
+import { sfdcConnFromRequest } from "../common/oauth_helper"
 
 interface CampaignMember {
-  ContactId?: string;
-  LeadId?: string;
-  CampaignId: string;
-  Status?: string;
+  ContactId?: string
+  LeadId?: string
+  CampaignId: string
+  Status?: string
 }
 
 export class SalesforceCampaignsSendData {
   async sendData(
     request: Hub.ActionRequest,
     memberIds: string[],
-    tokens: Tokens
+    tokens: Tokens,
   ) {
     // const sfdcConn = await salesforceLogin(request);
-    const sfdcConn = await sfdcConnFromRequest(request, tokens);
+    const sfdcConn = await sfdcConnFromRequest(request, tokens)
 
     // with refresh token we can get new access token and update the state without forcing a re-login
-    let newTokens = { ...tokens };
+    let newTokens = { ...tokens }
     sfdcConn.on("refresh", (newAccessToken: string) => {
       newTokens = {
         access_token: newAccessToken,
         refresh_token: sfdcConn.refreshToken,
-      };
-    });
+      }
+    })
 
-    let campaignId: string;
+    let campaignId: string
 
     switch (request.formParams.create_or_append) {
       case "create":
         const newCampaign = await sfdcConn
           .sobject("Campaign")
-          .create({ Name: request.formParams.campaign_name });
+          .create({ Name: request.formParams.campaign_name })
 
         if (!newCampaign.success) {
           throw new Error(
-            `Campaign creation error: ${JSON.stringify(newCampaign.errors)}`
-          );
+            `Campaign creation error: ${JSON.stringify(newCampaign.errors)}`,
+          )
         } else {
-          campaignId = newCampaign.id;
+          campaignId = newCampaign.id
         }
 
-        break;
+        break
       case "append":
-        campaignId = request.formParams.campaign_name!;
+        campaignId = request.formParams.campaign_name!
     }
 
     const memberType =
-      request.formParams.member_type === "lead" ? "LeadId" : "ContactId";
+      request.formParams.member_type === "lead" ? "LeadId" : "ContactId"
     const memberList = memberIds.map((m) => {
       return {
         [memberType]: m,
         CampaignId: campaignId,
         Status: request.formParams.member_status,
-      };
-    });
+      }
+    })
 
     // POST request with sObject Collections to execute multiple records in a single request
-    // https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_composite_sobjects_collections_create.htm
+    // https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta
+    // /api_rest/resources_composite_sobjects_collections_create.htm
 
     // The entire request (up to 200 records per chunk) counts as a single API call toward API limits.
     // This resource is available in API v42.0+ and later (released Spring 2018)
 
-    // For Salesforce Professional and Enterprise, each organization receives a total of 1k API calls per-user in a 24-hour period
-    // https://developer.salesforce.com/docs/atlas.en-us.salesforce_app_limits_cheatsheet.meta/salesforce_app_limits_cheatsheet/salesforce_app_limits_platform_api.htm
+    // For Salesforce Professional and Enterprise, each organization receives a total
+    // of 1k API calls per-user in a 24-hour period
+    // https://developer.salesforce.com/docs/atlas.en-us.salesforce_app_limits_cheatsheet.meta
+    // /salesforce_app_limits_cheatsheet/salesforce_app_limits_platform_api.htm
 
     // bottom limit = 1,000 requests * 200 members = 200,000 members in 24h period per user
-    const memberGrouped = this.chunk(memberList, CHUNK_SIZE);
-
-    let memberErrors: jsforce.ErrorResult[] = [];
+    const memberGrouped = this.chunk(memberList, CHUNK_SIZE)
+    const memberErrors: jsforce.ErrorResult[] = []
 
     // jsforce uses function overloading for the create function, which causes issues resolving the
     // function signature as the compiler picks the first definition in declaration order, which is
@@ -79,15 +81,15 @@ export class SalesforceCampaignsSendData {
     const records: any = await Promise.all(
       // todo replicate promise.allSettled behaviour in ES6
       memberGrouped.map(async (m) => {
-        return await sfdcConn.sobject("CampaignMember").create(m);
-      })
-    );
+        return await sfdcConn.sobject("CampaignMember").create(m)
+      }),
+    )
 
     records.map((group: jsforce.RecordResult[]) => {
       group.map((r) => {
-        !r.success ? memberErrors.push(r) : null;
-      });
-    });
+        !r.success ? memberErrors.push(r) : null
+      })
+    })
 
     if (memberErrors.length > 0) {
       const cleanErrors = memberErrors.map((members) =>
@@ -95,20 +97,20 @@ export class SalesforceCampaignsSendData {
           return {
             statusCode: e.statusCode,
             message: e.message,
-          };
-        })
-      );
-      throw new Error(JSON.stringify(cleanErrors));
+          }
+        }),
+      )
+      throw new Error(JSON.stringify(cleanErrors))
     }
 
-    return newTokens;
+    return newTokens
   }
 
   chunk(items: CampaignMember[], size: number) {
-    const chunks = [];
-    while (items.length) {
-      chunks.push(items.splice(0, size));
+    const chunks = []
+    while (items.length > 0) {
+      chunks.push(items.splice(0, size))
     }
-    return chunks;
+    return chunks
   }
 }
