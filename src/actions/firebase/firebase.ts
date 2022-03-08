1aaea632adbd746287dee0f5cceee4a914fe6e6a
Aatish-Molasi <molasi@google.com>
2022-03-08 13:29:04 +0000

    Formatting updates

--- src/actions/firebase/firebase.ts
+++ src/actions/firebase/firebase.ts
@@ -10,17 +10,17 @@
 	  storageBucket = 'gs://looker-9d4d2.appspot.com'
 	  contentType = "image/jpeg"
 	  notificationOptions = {
-          priority: "high",
-          timeToLive: 60 * 60 * 24,
-          contentAvailable: true,
-          mutableContent: true
+      priority: "high",
+      timeToLive: 60 * 60 * 24,
+      contentAvailable: true,
+      mutableContent: true
 	  }
 	  static firebaseAdmin = admin.initializeApp({
 								credential: admin.credential.cert({projectId: process.env.FIREBASE_PROJECT_ID,
 																   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 																   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
 																   }),
-								databaseURL: "https://looker-9d4d2.firebaseio.com",
+								databaseURL: process.env.FIREBASE_DATABASE,
 							});
 	  params = []
 	  supportedActionTypes = [Hub.ActionType.Query]
@@ -29,122 +29,123 @@
 	  supportedVisualizationFormattings = [Hub.ActionVisualizationFormatting.Noapply]
 
 	  async execute(request: Hub.ActionRequest) {
-        let response = new Hub.ActionResponse({success: true})
-        try {
-            let data: any = {}
-            if (request.formParams.data) {
-              data = request.formParams.data
-              if (!data.alertId) {
-                throw "Need Valid AlertId."
-              }
-            } else {
-              throw "Need valid notification data."
-            }
-            let imageName = await this.uploadImage(request)
-            await this.verifyAndSendMessage(request.formParams, data, imageName)
-        } catch(e) {
-            console.log(e)
-            response = new Hub.ActionResponse({success: false, message: e})
+      let response = new Hub.ActionResponse({success: true})
+      try {
+        let data: any = {}
+        if (request.formParams.data) {
+          data = request.formParams.data
+          if (!data.alertId) {
+            throw "Need Valid AlertId."
+          }
+        } else {
+          throw "Need valid notification data."
         }
-        return new Hub.ActionResponse(response)
+        let imageName = await this.uploadImage(request)
+        await this.verifyAndSendMessage(request.formParams, imageName)
+      } catch(e) {
+          response = new Hub.ActionResponse({success: false, message: e})
+      }
+      return new Hub.ActionResponse(response)
 	  }
 
-    async verifyAndSendMessage(params: Hub.ParamMap, data: any, imageName: string): Promise<any> {
-        return new Promise(async (resolve, reject) => {
-            if (!params.title) {
-              reject("Needs a valid title.")
-            }
-            if (!params.deviceIds) {
-              resolve()
-            }
-            let notification: any = {title: params.title}
-            let notificationData: any = data
-            notificationData.imageUrl = imageName
-            if (params.timeToLive) {
-              this.notificationOptions.timeToLive = parseFloat(params.timeToLive)
-            }
-            if (params.priority) {
-              this.notificationOptions.priority = params.priority
-            }
-            try {
-                const userObj = JSON.parse(params.deviceIds ?? "[]") as any[]
-                let deviceIdObject = params.deviceIds as unknown as any[]
-                console.log("devices : " + deviceIdObject)
-                if (deviceIdObject.length == 0) {
-                  resolve()
+    async verifyAndSendMessage(params: Hub.ParamMap, imageName: string): Promise<any> {
+      return new Promise(async (resolve, reject) => {
+        let data: any = params.data
+        if (!params.title) {
+          reject("Needs a valid title.")
+        }
+        if (!params.deviceIds) {
+          resolve()
+        }
+        let notification: any = {title: params.title}
+        let notificationData: any = data
+        notificationData.imageUrl = imageName
+        if (params.timeToLive) {
+          this.notificationOptions.timeToLive = parseFloat(params.timeToLive)
+        }
+        if (params.priority) {
+          this.notificationOptions.priority = params.priority
+        }
+        try {
+          const userObj = JSON.parse(params.deviceIds ?? "[]") as any[]
+          let deviceIdObject = params.deviceIds as unknown as any[]
+          if (deviceIdObject.length == 0) {
+            resolve()
+          }
+          await userObj.forEach(async (userDevices) => {
+            let devices = userDevices as any[]
+            await devices.forEach(async (device, index) => {
+              if (device.device_id && device.user_id) {
+                let deviceId = device.device_id.toString()
+                notificationData.userId = device.user_id.toString()
+                let payload = {
+                  notification: notification,
+                  data: notificationData
+                };
+                try {
+                  await this.sendMessageToDevice(deviceId, payload, this.notificationOptions)
                 }
-                await userObj.forEach(async (userDevices) => {
-                    let devices = userDevices as any[]
-                    await devices.forEach(async (device, index) => {
-                        if (device.device_id && device.user_id) {
-                          let deviceId = device.device_id.toString()
-                          notificationData.userId = device.user_id.toString()
-                          let payload = {
-                              notification: notification,
-                              data: notificationData
-                          };
-                          try {
-                            await this.sendMessageToDevice(deviceId, payload, this.notificationOptions)
-                          }
-                          catch (error) {
-                            reject(error)
-                          }
-                        }
-                        if (index === devices.length -1) {
-                          resolve()
-                        }
-                    })
-                });
-            } catch (error) {
-                reject(error)
-            }
-       })
-       .catch((error) => {
-           throw error
-       })
+                catch (error) {
+                  reject(error)
+                }
+              }
+              if (index === devices.length -1) {
+                resolve()
+              }
+            })
+          });
+        } catch (error) {
+          reject(error)
+        }
+      })
+      .catch((error) => {
+        throw error
+      })
     }
 
     async sendMessageToDevice(deviceId: string, payload: admin.messaging.MessagingPayload, options?: admin.messaging.MessagingOptions): Promise<any> {
-        return new Promise((resolve, reject) => {
-            admin.messaging().sendToDevice(deviceId, payload, options)
-                .then( (_: any) => {
-                    resolve()
-                })
-                .catch( (error: any) => {
-                    reject(error.message)
-                });
-        })
-        .catch((error) => {
-          throw error
-        })
+      return new Promise((resolve, reject) => {
+        admin.messaging().sendToDevice(deviceId, payload, options)
+          .then( (_: any) => {
+            resolve()
+          })
+          .catch( (error: any) => {
+            reject(error.message)
+          });
+      })
+      .catch((error) => {
+        throw error
+      });
     }
 
     async uploadImage(request: Hub.ActionRequest): Promise<any> {
-        let data = request.formParams.data as any
-        console.log(data)
+      let data = request.formParams.data as any
 
-        try {
-            if (request.attachment?.dataBuffer) {
-                    return new Promise((resolve, reject) => {
-                        let dateString = new Date().toISOString()
-                        let alertId = data.alertId ?? ""
-                        const destFileName = dateString + alertId;
-                        const fileCloud = admin.storage().bucket(this.storageBucket).file(destFileName);
-                            fileCloud.save(request.attachment?.dataBuffer!, {
-                                metadata: {
-                                    contentType: this.contentType
-                                }
-                             }, (err) => {
-                                if (err) {
-                                    reject(err)
-                                }
-                                resolve(destFileName)
-                        });
-                })
-            }
-        } catch (e) {
-            return
+      try {
+        if (request.attachment?.dataBuffer) {
+          return new Promise((resolve, reject) => {
+            let dateString = new Date().toISOString()
+            let alertId = data.alertId ?? ""
+            const destFileName = dateString + alertId;
+            const fileCloud = admin.storage().bucket(this.storageBucket).file(destFileName);
+              fileCloud.save(request.attachment?.dataBuffer!, {
+                metadata: {
+                  contentType: this.contentType
+                }
+               }, (err) => {
+                if (err) {
+                  reject(err)
+                }
+                resolve(destFileName)
+            });
+          })
+        } else {
+          return
         }
+
+      } catch (e) {
+        return
+      }
     }
 
     async form() {
--- src/actions/firebase/test_firebase.ts
+++ src/actions/firebase/test_firebase.ts
@@ -1,29 +1,34 @@
 import * as chai from "chai"
-// import * as sinon from "sinon"
+import * as sinon from "sinon"
 
 import * as Hub from "../../hub"
 
 import { FirebaseAction } from "./firebase"
 
 const action = new FirebaseAction()
-// const stubFilename = "stubSuggestedFilename"
 
-// function expectFirebaseMatch(request: Hub.ActionRequest, match: any) {
+// function expectFirebaseMatch(request: Hub.ActionRequest, uploadImageMatch: any, sendMessageMatch: any) {
 //
-//   const sendSpy = sinon.spy(async () => Promise.resolve())
+//   const sendSpyUploadImage = sinon.spy(async () => {
+//     Promise.resolve("image")
+//     })
+//   const sendSpySendMessage = sinon.spy(async () => Promise.resolve())
 //
-//   const stubClient = sinon.stub(action as any, "sendMessageToDevice")
+//   const stubClientUploadImage = sinon.stub(action as any, "uploadImage")
 //     .callsFake(() => ({
-//       send: sendSpy,
+//       uploadImage: sendSpyUploadImage,
 //     }))
 //
-//   const stubSuggestedFilename = sinon.stub(request as any, "suggestedFilename")
-//     .callsFake(() => stubFilename)
+//   const stubClientSendMessage = sinon.stub(action as any, "verifyAndSendMessage")
+//     .callsFake(() => ({
+//       verifyAndSendMessage: sendSpySendMessage,
+//     }))
 //
 //   return chai.expect(action.execute(request)).to.be.fulfilled.then(() => {
-//     chai.expect(sendSpy).to.have.been.calledWithMatch(match)
-//     stubClient.restore()
-//     stubSuggestedFilename.restore()
+//     chai.expect(sendSpyUploadImage).to.have.been.calledWithMatch(uploadImageMatch)
+//     chai.expect(sendSpySendMessage).to.have.been.calledWithMatch(sendMessageMatch)
+//     stubClientUploadImage.restore()
+//     stubClientSendMessage.restore()
 //   })
 // }
 
@@ -56,9 +61,11 @@ describe(`${action.constructor.name} unit tests`, () => {
     it("no errors if there is valid request", () => {
       const response = new Hub.ActionResponse({success: true})
       const request = new Hub.ActionRequest()
-      request.formParams = {title: "title", "data": {"alertId": "123"}}
+      const alertIdData = {"alertId": "123"}
+      request.formParams = {title: "title", data: alertIdData as any}
       request.attachment = {}
       request.attachment.dataBuffer = Buffer.from("1,2,3,4", "utf8")
+//       return expectFirebaseMatch(request, {request: request}, {params: request.formParams, image: "image"})
       return chai.expect(action.execute(request)).to.eventually
         .deep.equal(response)
     })
