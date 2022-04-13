0b62233b42c0ceea666afe13140705e996cd31af
Phillip Peralez <peralez@google.com>
2022-04-13 15:39:55 +0000

    Readd lib/ directory to make actions imported and run through custom-action-hub work

---
+++ lib/actions/google/common/missing_auth_error.d.ts
@@ -0,0 +1,3 @@
+export declare class MissingAuthError extends Error {
+    constructor(message?: string);
+}
---
+++ lib/actions/google/common/missing_required_params_error.d.ts
@@ -0,0 +1,3 @@
+export declare class MissingRequiredParamsError extends Error {
+    constructor(message?: string);
+}
