21675bb9d0c0bf1de048d5f7520de5b87eadcf60
Dilip Kola <kdilipkola@gmail.com>
2024-01-03 10:21:29 +0530

    chore: fix tslint issues

--- src/actions/rudderstack/test_rudderstack.ts
+++ src/actions/rudderstack/test_rudderstack.ts
@@ -1,27 +1,27 @@
-import * as chai from "chai";
-import * as sinon from "sinon";
+import * as chai from "chai"
+import * as sinon from "sinon"
 
-import * as Hub from "../../hub";
-import * as apiKey from "../../server/api_key";
-import Server from "../../server/server";
-import { RudderAction } from "./rudderstack";
+import * as Hub from "../../hub"
+import * as apiKey from "../../server/api_key"
+import Server from "../../server/server"
+import { RudderAction } from "./rudderstack"
 
-const action = new RudderAction();
-action.executeInOwnProcess = false;
+const action = new RudderAction()
+action.executeInOwnProcess = false
 
 function expectRudderMatch(request: Hub.ActionRequest, match: any) {
-  const rudderCallSpy = sinon.spy();
+  const rudderCallSpy = sinon.spy()
   const stubClient = sinon
     .stub(action as any, "rudderClientFromRequest")
     .callsFake(() => {
-      return { identify: rudderCallSpy, flush: (cb: () => void) => cb() };
-    });
+      return { identify: rudderCallSpy, flush: (cb: () => void) => cb() }
+    })
   const stubAnon = sinon
     .stub(action as any, "generateAnonymousId")
-    .callsFake(() => "stubanon");
+    .callsFake(() => "stubanon")
 
-  const now = new Date();
-  const clock = sinon.useFakeTimers(now.getTime());
+  const now = new Date()
+  const clock = sinon.useFakeTimers(now.getTime())
 
   const baseMatch = {
     traits: {},
@@ -32,70 +32,70 @@ function expectRudderMatch(request: Hub.ActionRequest, match: any) {
       },
     },
     timestamp: now,
-  };
-  const merged = { ...baseMatch, ...match };
+  }
+  const merged = { ...baseMatch, ...match }
   return chai
     .expect(action.validateAndExecute(request))
     .to.be.fulfilled.then(() => {
-      chai.expect(rudderCallSpy).to.have.been.calledWithExactly(merged);
-      stubClient.restore();
-      stubAnon.restore();
-      clock.restore();
-    });
+      chai.expect(rudderCallSpy).to.have.been.calledWithExactly(merged)
+      stubClient.restore()
+      stubAnon.restore()
+      clock.restore()
+    })
 }
 
 describe(`${action.constructor.name} unit tests`, () => {
   describe("action", () => {
     it("works with user_id", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             fields: { dimensions: [{ name: "coolfield", tags: ["user_id"] }] },
             data: [{ coolfield: { value: "funvalue" } }],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: "funvalue",
         anonymousId: null,
-      });
-    });
+      })
+    })
 
     it("works with email", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             fields: { dimensions: [{ name: "coolfield", tags: ["email"] }] },
             data: [{ coolfield: { value: "funvalue" } }],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         anonymousId: "stubanon",
         userId: null,
         traits: { email: "funvalue" },
-      });
-    });
+      })
+    })
 
     it("works with pivoted values", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -105,27 +105,27 @@ describe(`${action.constructor.name} unit tests`, () => {
             },
             data: [
               {
-                coolfield: { value: "funvalue" },
+                "coolfield": { value: "funvalue" },
                 "users.count": { f: { value: 1 }, z: { value: 3 } },
               },
             ],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: "funvalue",
         anonymousId: null,
         traits: { "users.count": [{ f: 1 }, { z: 3 }] },
-      });
-    });
+      })
+    })
 
     it("works with email and user id", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -141,23 +141,23 @@ describe(`${action.constructor.name} unit tests`, () => {
                 coolid: { value: "id" },
               },
             ],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: "id",
         traits: { email: "email@email.email" },
         anonymousId: null,
-      });
-    });
+      })
+    })
 
     it("works with email, user id and anonymous id", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -175,23 +175,23 @@ describe(`${action.constructor.name} unit tests`, () => {
                 coolanonymousid: { value: "anon_id" },
               },
             ],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: "id",
         traits: { email: "email@email.email" },
         anonymousId: "anon_id",
-      });
-    });
+      })
+    })
 
     it("works with email, user id and anonymous id and trait", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -211,9 +211,9 @@ describe(`${action.constructor.name} unit tests`, () => {
                 cooltrait: { value: "funtrait" },
               },
             ],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: "id",
         traits: {
@@ -221,16 +221,16 @@ describe(`${action.constructor.name} unit tests`, () => {
           cooltrait: "funtrait",
         },
         anonymousId: "anon_id",
-      });
-    });
+      })
+    })
 
     it("works with user id and anonymous id", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -246,22 +246,22 @@ describe(`${action.constructor.name} unit tests`, () => {
                 coolanonymousid: { value: "anon_id" },
               },
             ],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: "id",
         anonymousId: "anon_id",
-      });
-    });
+      })
+    })
 
     it("works with anonymous id", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -271,22 +271,22 @@ describe(`${action.constructor.name} unit tests`, () => {
               ],
             },
             data: [{ coolanonymousid: { value: "anon_id" } }],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: null,
         anonymousId: "anon_id",
-      });
-    });
+      })
+    })
 
     it("doesn't send hidden fields", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
@@ -304,16 +304,16 @@ describe(`${action.constructor.name} unit tests`, () => {
                 nonhiddenfield: { value: "nonhiddenvalue" },
               },
             ],
-          })
+          }),
         ),
-      };
+      }
       request.scheduledPlan = {
         query: {
           vis_config: {
             hidden_fields: ["hiddenfield"],
           },
         },
-      } as any;
+      } as any
       return expectRudderMatch(request, {
         anonymousId: "stubanon",
         userId: null,
@@ -321,82 +321,82 @@ describe(`${action.constructor.name} unit tests`, () => {
           email: "funvalue",
           nonhiddenfield: "nonhiddenvalue",
         },
-      });
-    });
+      })
+    })
 
     it("works with null user_ids", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             fields: { dimensions: [{ name: "coolfield", tags: ["user_id"] }] },
             data: [{ coolfield: { value: null } }],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         userId: null,
         anonymousId: "stubanon",
-      });
-    });
+      })
+    })
 
     it("works with ran_at", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             fields: { dimensions: [{ name: "coolfield", tags: ["email"] }] },
             ran_at: "2017-07-28T02:25:19+00:00",
             data: [{ coolfield: { value: "funvalue" } }],
-          })
+          }),
         ),
-      };
+      }
       return expectRudderMatch(request, {
         anonymousId: "stubanon",
         userId: null,
         timestamp: new Date("2017-07-28T02:25:19+00:00"),
         traits: { email: "funvalue" },
-      });
-    });
+      })
+    })
 
     it("errors if the input has no attachment", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       return chai
         .expect(action.validateAndExecute(request))
         .to.eventually.be.rejectedWith(
-          "A streaming action was sent incompatible data. The action must have a download url or an attachment."
-        );
-    });
+          "A streaming action was sent incompatible data. The action must have a download url or an attachment.",
+        )
+    })
 
     it("errors if the query response has no fields", (done) => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             data: [{ coolfield: { value: "funvalue" } }],
-          })
+          }),
         ),
-      };
+      }
       chai
         .expect(action.validateAndExecute(request))
         .to.eventually.deep.equal({
@@ -406,24 +406,24 @@ describe(`${action.constructor.name} unit tests`, () => {
           refreshQuery: false,
           validationErrors: [],
         })
-        .and.notify(done);
-    });
+        .and.notify(done)
+    })
 
     it("errors if there is no tagged field", (done) => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.params = {
         rudder_write_key: "mykey",
         rudder_server_url: "http://localhost:8080",
-      };
+      }
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             fields: { dimensions: [{ name: "coolfield", tags: [] }] },
             data: [{ coolfield: { value: "funvalue" } }],
-          })
+          }),
         ),
-      };
+      }
       chai
         .expect(action.validateAndExecute(request))
         .to.eventually.deep.equal({
@@ -433,33 +433,33 @@ describe(`${action.constructor.name} unit tests`, () => {
           refreshQuery: false,
           validationErrors: [],
         })
-        .and.notify(done);
-    });
+        .and.notify(done)
+    })
 
     it("errors if there is no write key", () => {
-      const request = new Hub.ActionRequest();
-      request.type = Hub.ActionType.Query;
+      const request = new Hub.ActionRequest()
+      request.type = Hub.ActionType.Query
       request.attachment = {
         dataBuffer: Buffer.from(
           JSON.stringify({
             fields: { dimensions: [{ name: "coolfield", tags: ["user_id"] }] },
             data: [],
-          })
+          }),
         ),
-      };
+      }
       return chai
         .expect(action.validateAndExecute(request))
         .to.eventually.be.rejectedWith(
-          `Required setting "Rudder Write Key" not specified in action settings.`
-        );
-    });
-  });
+          `Required setting "Rudder Write Key" not specified in action settings.`,
+        )
+    })
+  })
 
   describe("form", () => {
     it("has no form", () => {
-      chai.expect(action.hasForm).equals(false);
-    });
-  });
+      chai.expect(action.hasForm).equals(false)
+    })
+  })
 
   describe.skip("asJSON", () => {
     it("supported format is json_detail on lookerVersion 6.0 and below", (done) => {
@@ -473,32 +473,32 @@ describe(`${action.constructor.name} unit tests`, () => {
         .set("Authorization", 'Token token="foo"')
         .set("User-Agent", "LookerOutgoingWebhook/6.0.0")
         .end((_err, res) => {
-          chai.expect(res).to.have.status(200);
+          chai.expect(res).to.have.status(200)
           chai
             .expect(res.body)
-            .to.deep.include({ supported_formats: ["json_detail"] });
-          stub.restore();
-          done();
-        });
-    });
+            .to.deep.include({ supported_formats: ["json_detail"] })
+          stub.restore()
+          done()
+        })
+    })
 
     it("supported format is json_detail_lite_stream on lookerVersion 6.2 and above", (done) => {
       const stub = sinon
         .stub(apiKey, "validate")
-        .callsFake((k: string) => k === "foo");
+        .callsFake((k: string) => k === "foo")
       chai
         .request(new Server().app)
         .post("/actions/rudder_event")
         .set("Authorization", 'Token token="foo"')
         .set("User-Agent", "LookerOutgoingWebhook/6.2.0")
         .end((_err, res) => {
-          chai.expect(res).to.have.status(200);
+          chai.expect(res).to.have.status(200)
           chai.expect(res.body).to.deep.include({
             supported_formats: ["json_detail_lite_stream"],
-          });
-          stub.restore();
-          done();
-        });
-    });
-  });
-});
+          })
+          stub.restore()
+          done()
+        })
+    })
+  })
+})
