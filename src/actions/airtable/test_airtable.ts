e607d90364726d010a883e9a347ef308c920b302
Jon Ting <jonting@google.com>
2024-06-04 01:47:19 +0000

    Refactor airtable token refresh
    This change will move airtable token refresh to when the attempt
    to reach airtable fails instead of on each request.
    
    This aligns with the airtable style and should work better and
    increase availability.

--- src/actions/airtable/test_airtable.ts
+++ src/actions/airtable/test_airtable.ts
@@ -76,6 +76,36 @@ describe(`${action.constructor.name} unit tests`, () => {
         .be.rejectedWith("Missing Airtable base or table.")
     })
 
+    it("errors if there is no fields in dataJSON", () => {
+      const request = new Hub.ActionRequest()
+      request.formParams = request.formParams = {
+        base: "mybase",
+        table: "mytable",
+      }
+      request.attachment = {dataJSON: {
+          data: [{"coolview.coolfield": {value: "funvalue"}}],
+        }}
+      return chai.expect(action.execute(request)).to.eventually
+          .be.rejectedWith("Request payload is an invalid format.")
+    })
+
+    it("errors if there is no data in dataJSON", () => {
+      const request = new Hub.ActionRequest()
+      request.formParams = request.formParams = {
+        base: "mybase",
+        table: "mytable",
+      }
+      request.attachment = {dataJSON: {
+          fields: {
+            dimensions: [
+              {name: "coolview.coolfield", label_short: "cool field", tags: ["user_id"]},
+            ],
+          },
+        }}
+      return chai.expect(action.execute(request)).to.eventually
+          .be.rejectedWith("Request payload is an invalid format.")
+    })
+
     it("sends right body", () => {
       const request = new Hub.ActionRequest()
       request.formParams = {
@@ -160,6 +190,26 @@ describe(`${action.constructor.name} unit tests`, () => {
       })
     })
 
+    it("refreshes on oauth failure", () => {
+      const request = new Hub.ActionRequest()
+      request.formParams = {
+        base: "mybase",
+        table: "mytable",
+      }
+      request.attachment = {dataJSON: {
+          fields: {
+            dimensions: [
+              {name: "coolview.coolfield", tags: ["user_id"]},
+            ],
+          },
+          data: [{"coolview.coolfield": {value: "funvalue"}}],
+        }}
+      return expectWebhookMatch(request,
+          request.formParams.base,
+          request.formParams.table,
+          {"coolview.coolfield": "funvalue"})
+    })
+
   })
 
   describe("form", () => {
@@ -170,8 +220,38 @@ describe(`${action.constructor.name} unit tests`, () => {
 
     it("has form with base and table param", (done) => {
       const request = new Hub.ActionRequest()
-      request.params.state_json = "{\"tokens\": {\"refresh_token\": \"token\"}}"
+      request.params.state_json = "{\"tokens\":{\"refresh_token\":\"token\"}}"
       gaxiosStub.resolves({data: {access_token: "test", refresh_token: "lol"}} as any)
+
+      const form = action.validateAndFetchForm(request)
+      chai.expect(form).to.eventually.deep.equal({
+        fields: [{
+          label: "Airtable Base",
+          name: "base",
+          required: true,
+          type: "string",
+        }, {
+          label: "Airtable Table",
+          name: "table",
+          required: true,
+          type: "string",
+        }],
+        state: {
+          data: "{\"tokens\":{\"refresh_token\":\"token\"}}",
+        },
+      }).and.notify(done)
+    })
+
+    it("succeeds after failing oauth once", (done) => {
+      const request = new Hub.ActionRequest()
+      request.params.state_json = "{\"tokens\": {\"refresh_token\": \"token\"}}"
+      gaxiosStub
+          .onCall(0)
+          .throws("oauthTestFailure")
+          .onCall(1)
+          .resolves({data: {access_token: "test", refresh_token: "lol"}} as any)
+          .onCall(2)
+          .resolves({data: "responseToCheckBaseList"})
 
       const form = action.validateAndFetchForm(request)
       chai.expect(form).to.eventually.deep.equal({
