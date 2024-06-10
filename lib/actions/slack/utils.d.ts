de6cb2041c90d3a34dbe661c54ada73a200b852b
Eric Dunbar <edunbar@google.com>
2024-06-10 20:29:44 +0000

    ran yarn build

--- lib/actions/slack/utils.d.ts
+++ lib/actions/slack/utils.d.ts
@@ -2,7 +2,6 @@ import { WebClient } from "@slack/web-api";
 import * as Hub from "../../hub";
 import { ActionFormField } from "../../hub";
 export declare const API_LIMIT_SIZE = 1000;
-export declare const setErrorResponse: (error: any) => Hub.Error;
 export declare const getDisplayedFormFields: (slack: WebClient, channelType: string) => Promise<ActionFormField[]>;
 export declare const handleExecute: (request: Hub.ActionRequest, slack: WebClient) => Promise<Hub.ActionResponse>;
 export declare const displayError: {
