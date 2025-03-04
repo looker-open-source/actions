7342ee825dcee1fb419e8facabcd510e2426fee9
Itzayana Prado <itzayanaprado@google.com>
2025-03-04 20:42:53 +0000

    feat: update google ads to version v19

--- src/actions/google/ads/lib/api_client.ts
+++ src/actions/google/ads/lib/api_client.ts
@@ -145,7 +145,7 @@ export class GoogleAdsApiClient {
         url,
         data,
         headers,
-        baseURL: "https://googleads.googleapis.com/v17/",
+        baseURL: "https://googleads.googleapis.com/v19/",
       })
 
       if (process.env.ACTION_HUB_DEBUG) {
