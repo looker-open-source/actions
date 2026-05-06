fceae13b2a0eb704496c93cbae52c9baaf42be72
Manav Sanghavi <manavs@google.com>
2026-05-06 15:12:42 -0700

    Uncomment Marketo tests and fix them instead

--- src/actions/marketo/test_marketo.ts
+++ src/actions/marketo/test_marketo.ts
@@ -212,19 +212,23 @@ import { MarketoTransaction } from "./marketo_transaction"
         listRemoveLeadsFromList: spies[3],
       }
 
-      sinon.stub(MarketoTransaction.prototype, "marketoClientFromRequest").callsFake(() => {
-        return {
-          lead: {
-            createOrUpdate: spy.leadCreateOrUpdate,
-          },
-          campaign: {
-            request: spy.campaignRequest,
-          },
-          list: {
-            addLeadsToList: spy.listAddLeadsToList,
-            removeLeadsFromList: spy.listRemoveLeadsFromList,
-          },
-        }
+      // Wrap stubbing in before() to ensure it is created after any global sinon.restore()
+      // calls from previous test files (like test_server.ts) have run.
+      before(() => {
+        sinon.stub(MarketoTransaction.prototype, "marketoClientFromRequest").callsFake(() => {
+          return {
+            lead: {
+              createOrUpdate: spy.leadCreateOrUpdate,
+            },
+            campaign: {
+              request: spy.campaignRequest,
+            },
+            list: {
+              addLeadsToList: spy.listAddLeadsToList,
+              removeLeadsFromList: spy.listRemoveLeadsFromList,
+            },
+          }
+        })
       })
 
       it("sends all the data to Marketo for the legacy request format", () => {
